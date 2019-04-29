37630d16c6ee0f4cc49d199575fe1d50add81ca0
Mathis Hofer <hofer@puzzle.ch>
2019-04-29 14:44:48 +0200

    Add settings.js and SettingsService #40

---
+++ src/settings.example.js
@@ -0,0 +1,8 @@
+// Rename this file to settings.js and adjust the settings
+
+window.absenzenmanagement = window.absenzenmanagement || {};
+
+window.absenzenmanagement.settings = {
+  // API base URL without trailing slash
+  apiUrl: 'https://eventotest.api'
+};
