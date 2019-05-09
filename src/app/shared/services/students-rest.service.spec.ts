7b48eba500128bcd9f6d35e9252fcdde26ac1ae8
Claudia Asti <asti@puzzle.ch>
2019-05-09 11:35:54 +0200

    Add a unit test for the get legal representatives service method #36

--- src/app/shared/services/students-rest.service.spec.ts
+++ src/app/shared/services/students-rest.service.spec.ts
@@ -2,12 +2,37 @@ import { TestBed } from '@angular/core/testing';
 
 import { buildTestModuleMetadata } from 'src/spec-helpers';
 import { StudentsRestService } from './students-rest.service';
+import { LegalRepresentative } from '../models/legal-representative.model';
+import { HttpTestingController } from '@angular/common/http/testing';
 
 describe('StudentsRestService', () => {
-  beforeEach(() => TestBed.configureTestingModule(buildTestModuleMetadata()));
+  let service: StudentsRestService;
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule(buildTestModuleMetadata());
+    service = TestBed.get(StudentsRestService);
+    httpTestingController = TestBed.get(HttpTestingController);
+  });
+
+  afterEach(() => httpTestingController.verify());
 
   it('should be created', () => {
-    const service: StudentsRestService = TestBed.get(StudentsRestService);
     expect(service).toBeTruthy();
+  });
+
+  describe('.getLegalRepresentatives', () => {
+    it('requests the legal representatives of a given student', () => {
+      service.getLegalRepresentatives(39361).subscribe(result => {
+        expect(result).toEqual([
+          LegalRepresentative.from({ id: 54425 }),
+          LegalRepresentative.from({ id: 56200 })
+        ]);
+      });
+
+      httpTestingController
+        .expectOne('https://eventotest.api/Students/39361/LegalRepresentatives')
+        .flush([{ id: 54425 }, { id: 56200 }]);
+    });
   });
 });
