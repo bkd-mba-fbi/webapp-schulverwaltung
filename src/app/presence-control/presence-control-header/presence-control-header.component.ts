271e2b58f6b0df5b950a7199945a680b50d0df44
Mathis Hofer <hofer@puzzle.ch>
2019-05-13 13:54:40 +0200

    Rename presence control header css file to scss #36

--- src/app/presence-control/presence-control-header/presence-control-header.component.ts
+++ src/app/presence-control/presence-control-header/presence-control-header.component.ts
@@ -4,7 +4,7 @@ import { Lesson } from 'src/app/shared/models/lesson.model';
 @Component({
   selector: 'erz-presence-control-header',
   templateUrl: './presence-control-header.component.html',
-  styleUrls: ['./presence-control-header.component.css']
+  styleUrls: ['./presence-control-header.component.scss']
 })
 export class PresenceControlHeaderComponent implements OnInit {
   @Input() lesson: Lesson;
