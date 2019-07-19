b092d858b4ed33de7f22a7f29080ddc2118d7bb6
Claudia Asti <asti@puzzle.ch>
2019-07-19 15:08:07 +0200

    Move toggle presence type logic from entry to list #11

--- src/app/presence-control/components/presence-control-entry/presence-control-entry.component.ts
+++ src/app/presence-control/components/presence-control-entry/presence-control-entry.component.ts
@@ -8,15 +8,9 @@ import {
   Output,
   SimpleChanges
 } from '@angular/core';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ReplaySubject } from 'rxjs';
-import { take, tap } from 'rxjs/operators';
 import { PresenceControlEntry } from '../../models/presence-control-entry.model';
-import {
-  PresenceControlStateService,
-  ViewMode
-} from '../../services/presence-control-state.service';
-import { PresenceControlDialogComponent } from '../presence-control-dialog/presence-control-dialog.component';
+import { ViewMode } from '../../services/presence-control-state.service';
 
 @Component({
   selector: 'erz-presence-control-entry',
@@ -35,10 +29,7 @@ export class PresenceControlEntryComponent implements OnInit, OnChanges {
 
   private studentId$ = new ReplaySubject<number>(1);
 
-  constructor(
-    private modalService: NgbModal,
-    private stateService: PresenceControlStateService
-  ) {}
+  constructor() {}
 
   ngOnInit(): void {}
 
@@ -65,65 +56,7 @@ export class PresenceControlEntryComponent implements OnInit, OnChanges {
     return this.viewMode === ViewMode.List;
   }
 
-  open(entry: PresenceControlEntry): void {
-    this.stateService
-      .getBlockLessons(entry)
-      .pipe(
-        take(1),
-        tap(lessonPresences => console.log(lessonPresences))
-      )
-      .subscribe(lessonPresences => {
-        if (lessonPresences.length < 2) {
-          this.togglePresenceType.emit(entry);
-        } else {
-          // TODO
-          const modalRef = this.modalService.open(
-            PresenceControlDialogComponent
-          );
-          modalRef.componentInstance.entry = entry;
-          modalRef.componentInstance.blockLessonPresences = lessonPresences;
-          modalRef.result.then(
-            result => {
-              if (result === 'save') {
-                console.log('save');
-              }
-            },
-            () => {
-              console.log('close');
-            }
-          );
-        }
-      });
+  updatePresenceType(entry: PresenceControlEntry): void {
+    this.togglePresenceType.emit(entry);
   }
-
-  // updatePresenceType(dialog: any, entry: PresenceControlEntry): void {
-  //   this.selectionService.toggle(entry.lessonPresence);
-  //   if (entry.blockLessonPresences.length === 1) {
-  //     this.togglePresenceType.emit(entry);
-  //   } else {
-  //     this.modalService
-  //       .open(dialog, { ariaLabelledBy: 'modal-basic-title' })
-  //       .result.then(
-  //         result => {
-  //           if (result === 'save') {
-  //             this.selectionService.selectedIds$
-  //               .pipe(take(1))
-  //               .subscribe(selectedIds => {
-  //                 // TODO do not change the block lesson presences on the entry, emit object?
-  //                 entry.blockLessonPresences = entry.blockLessonPresences.filter(
-  //                   presence => selectedIds.includes(presence.LessonRef.Id)
-  //                 );
-  //                 this.togglePresenceType.emit(entry);
-  //               });
-  //           }
-  //           if (result === 'cancel') {
-  //             this.selectionService.clear();
-  //           }
-  //         },
-  //         () => {
-  //           this.selectionService.clear();
-  //         }
-  //       );
-  //   }
-  // }
 }
--- src/app/presence-control/components/presence-control-list/presence-control-list.component.ts
+++ src/app/presence-control/components/presence-control-list/presence-control-list.component.ts
@@ -1,6 +1,8 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { BehaviorSubject, combineLatest } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import { map, shareReplay, take, tap } from 'rxjs/operators';
+import { LessonPresence } from 'src/app/shared/models/lesson-presence.model';
 import { LessonPresencesUpdateService } from 'src/app/shared/services/lesson-presences-update.service';
 import { searchEntries } from 'src/app/shared/utils/search';
 import { spreadTuple } from '../../../shared/utils/function';
@@ -10,6 +12,7 @@ import {
   filterPreviouslyAbsentEntries,
   filterPreviouslyPresentEntries
 } from '../../utils/presence-control-entries';
+import { PresenceControlDialogComponent } from '../presence-control-dialog/presence-control-dialog.component';
 
 @Component({
   selector: 'erz-presence-control-list',
@@ -37,18 +40,85 @@ export class PresenceControlListComponent implements OnInit {
 
   constructor(
     public state: PresenceControlStateService,
-    private lessonPresencesUpdateService: LessonPresencesUpdateService
+    private lessonPresencesUpdateService: LessonPresencesUpdateService,
+    private modalService: NgbModal
   ) {}
 
   ngOnInit(): void {}
 
-  togglePresenceType(entry: PresenceControlEntry): void {
-    this.state.getNextPresenceType(entry).subscribe(newPresenceType =>
-      this.lessonPresencesUpdateService.updatePresenceTypes(
-        // TODO get selected block lesson presences from param
-        [entry.lessonPresence],
-        newPresenceType ? newPresenceType.Id : null
-      )
-    );
+  doTogglePresenceType(
+    entry: PresenceControlEntry,
+    lessonPresences: ReadonlyArray<LessonPresence>
+  ): void {
+    this.state
+      .getNextPresenceType(entry)
+      .subscribe(newPresenceType =>
+        this.lessonPresencesUpdateService.updatePresenceTypes(
+          lessonPresences,
+          newPresenceType ? newPresenceType.Id : null
+        )
+      );
   }
+
+  togglePresenceType(entry: PresenceControlEntry): void {
+    this.state
+      .getBlockLessons(entry)
+      .pipe(
+        take(1),
+        tap(lessonPresences => console.log(lessonPresences))
+      )
+      .subscribe(lessonPresences => {
+        if (lessonPresences.length < 2) {
+          this.doTogglePresenceType(entry, lessonPresences);
+        } else {
+          // TODO
+          const modalRef = this.modalService.open(
+            PresenceControlDialogComponent
+          );
+          modalRef.componentInstance.entry = entry;
+          modalRef.componentInstance.blockLessonPresences = lessonPresences;
+          modalRef.result.then(
+            result => {
+              if (result === 'save') {
+                console.log('save');
+              }
+            },
+            () => {
+              console.log('close');
+            }
+          );
+        }
+      });
+  }
+
+  // updatePresenceType(dialog: any, entry: PresenceControlEntry): void {
+  //   this.selectionService.toggle(entry.lessonPresence);
+  //   if (entry.blockLessonPresences.length === 1) {
+  //     this.togglePresenceType.emit(entry);
+  //   } else {
+  //     this.modalService
+  //       .open(dialog, { ariaLabelledBy: 'modal-basic-title' })
+  //       .result.then(
+  //         result => {
+  //           if (result === 'save') {
+  //             this.selectionService.selectedIds$
+  //               .pipe(take(1))
+  //               .subscribe(selectedIds => {
+  //                 // TODO do not change the block lesson presences on the entry, emit object?
+  //                 entry.blockLessonPresences = entry.blockLessonPresences.filter(
+  //                   presence => selectedIds.includes(presence.LessonRef.Id)
+  //                 );
+  //                 this.togglePresenceType.emit(entry);
+  //               });
+  //           }
+  //           if (result === 'cancel') {
+  //             this.selectionService.clear();
+  //           }
+  //         },
+  //         () => {
+  //           this.selectionService.clear();
+  //         }
+  //       );
+  //   }
+  // }
 }
