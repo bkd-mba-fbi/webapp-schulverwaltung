aee2bcb650cb9216ea9fa7dfcbd0054c068b187b
Claudia Asti <asti@puzzle.ch>
2019-08-26 11:54:07 +0200

    Remove duplicated import #29

--- src/app/shared/components/typeahead/typeahead.component.spec.ts
+++ src/app/shared/components/typeahead/typeahead.component.spec.ts
@@ -1,6 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { buildTestModuleMetadata } from 'src/spec-helpers';
 import { TypeaheadComponent } from './typeahead.component';
 import { buildTestModuleMetadata } from 'src/spec-helpers';
 
