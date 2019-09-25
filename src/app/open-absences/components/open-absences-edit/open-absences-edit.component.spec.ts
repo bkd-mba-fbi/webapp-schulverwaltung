214cfa152bb1c88f46a37b3c73816ba7ccc6db88
mwkp <sandro.scheidt@erz.be.ch>
2019-09-25 14:36:19 +0200

    #120 add storeMock CLX.LoginToken

--- src/app/open-absences/components/open-absences-edit/open-absences-edit.component.spec.ts
+++ src/app/open-absences/components/open-absences-edit/open-absences-edit.component.spec.ts
@@ -19,6 +19,9 @@ describe('OpenAbsencesEditComponent', () => {
       (key: string) => storeMock[key] || null
     );
 
+    storeMock['CLX.LoginToken'] =
+      'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJvYXV0aCIsImF1ZCI6Imh0dHBzOi8vZGV2NDIwMC8iLCJuYmYiOjE1NjkzOTM5NDMsImV4cCI6MTU2OTQwODM0MywidG9rZW5fcHVycG9zZSI6IlVzZXIiLCJzY29wZSI6IlR1dG9yaW5nIiwiY29uc3VtZXJfaWQiOiJkZXY0MjAwIiwidXNlcm5hbWUiOiJMMjQzMSIsImluc3RhbmNlX2lkIjoiR1ltVEVTVCIsImN1bHR1cmVfaW5mbyI6ImRlLUNIIiwicmVkaXJlY3RfdXJpIjoiaHR0cDovL2xvY2FsaG9zdDo0MjAwIiwiaWRfbWFuZGFudCI6IjIxMCIsImlkX3BlcnNvbiI6IjI0MzEiLCJmdWxsbmFtZSI6IlRlc3QgUnVkeSIsInJvbGVzIjoiTGVzc29uVGVhY2hlclJvbGU7Q2xhc3NUZWFjaGVyUm9sZSIsInRva2VuX2lkIjoiMzc0OSJ9.9lDju5CIIUaISRSz0x8k-kcF7Q6IhN_6HEMOlnsiDRA';
+
     TestBed.configureTestingModule(
       buildTestModuleMetadata({
         declarations: [OpenAbsencesEditComponent],
