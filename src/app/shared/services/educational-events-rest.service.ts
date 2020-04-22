aeaa456860fcc6330013209ed34f1424ed0c8f97
Claudia Asti <asti@puzzle.ch>
2020-04-22 16:40:52 +0200

    Adjust error message to resemble the one show in the rest error interceptor

--- src/app/shared/services/educational-events-rest.service.ts
+++ src/app/shared/services/educational-events-rest.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Inject } from '@angular/core';
+import { Injectable, Inject, Injector } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { SETTINGS, Settings } from '../../settings';
@@ -6,8 +6,10 @@ import { TypeaheadRestService } from './typeahead-rest.service';
 import { EducationalEvent } from '../models/educational-event.model';
 import { switchMap, map } from 'rxjs/operators';
 import { decodeArray } from '../utils/decode';
-import { Observable } from 'rxjs';
+import { Observable, of, EMPTY } from 'rxjs';
 import { DropDownItem } from '../models/drop-down-item.model';
+import { ToastrService } from 'ngx-toastr';
+import { TranslateService } from '@ngx-translate/core';
 
 @Injectable({
   providedIn: 'root',
@@ -15,7 +17,12 @@ import { DropDownItem } from '../models/drop-down-item.model';
 export class EducationalEventsRestService extends TypeaheadRestService<
   typeof EducationalEvent
 > {
-  constructor(http: HttpClient, @Inject(SETTINGS) settings: Settings) {
+  constructor(
+    http: HttpClient,
+    @Inject(SETTINGS) settings: Settings,
+    private translate: TranslateService,
+    private injector: Injector
+  ) {
     super(http, settings, EducationalEvent, 'EducationalEvents', 'Designation');
   }
 
@@ -47,11 +54,19 @@ export class EducationalEventsRestService extends TypeaheadRestService<
         switchMap(decodeArray(this.typeaheadCodec)),
         map((items) => {
           if (items.length === 0) {
-            throw new Error('educational event not found');
+            this.toastr.error(
+              this.translate.instant(`global.rest-errors.notfound-message`),
+              this.translate.instant(`global.rest-errors.notfound-title`)
+            );
+            return EMPTY;
           }
           return items[0];
         }),
         map((item) => ({ Key: item[this.idAttr], Value: item[this.labelAttr] }))
       );
+  }
+
+  private get toastr(): ToastrService {
+    return this.injector.get(ToastrService);
   }
 }
