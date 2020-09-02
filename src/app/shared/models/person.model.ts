6a85a266abd1fd82164941d3c970725d5f6f51b3
Claudia Asti <asti@puzzle.ch>
2020-09-02 13:50:21 +0200

    Enable missing properties on person model an display them on the profile

--- src/app/shared/models/person.model.ts
+++ src/app/shared/models/person.model.ts
@@ -1,5 +1,5 @@
 import * as t from 'io-ts';
-import { Option, Maybe } from './common-types';
+import { Option, Maybe, LocalDateTimeFromString } from './common-types';
 
 const Person = t.type({
   Id: t.number,
@@ -9,7 +9,7 @@ const Person = t.type({
   // FormOfAddressId: Option(t.number),
   // HomeCountry: Option(t.string),
   // HomeCountryId: Option(t.string),
-  // Nationality: Option(t.string),
+  Nationality: Option(t.string),
   // NationalityId: Option(t.number),
   AddressLine1: Option(t.string),
   AddressLine2: Option(t.string),
@@ -21,21 +21,21 @@ const Person = t.type({
   Email2: Maybe(t.string),
   FirstName: Option(t.string),
   Gender: Option(t.union([t.literal('M'), t.literal('F'), t.literal('X')])),
-  // HomeTown: Option(t.string),
+  HomeTown: Option(t.string),
   // IsEditable: t.boolean,
   // IsEmployee: t.boolean,
   LastName: Option(t.string),
   Location: Option(t.string),
   // MatriculationNumber: Option(t.string),
   // MiddleName: Option(t.string),
-  // NativeLanguage: Option(t.string),
+  NativeLanguage: Option(t.string),
   PhoneMobile: Option(t.string),
   PhonePrivate: Option(t.string),
   PhoneBusiness: Maybe(t.string),
   // Profession: Option(t.string),
-  // SocialSecurityNumber: Option(t.string),
-  // StayPermit: Option(t.string),
-  // StayPermitExpiry: Option(LocalDateTimeFromString),
+  SocialSecurityNumber: Option(t.string),
+  StayPermit: Option(t.string),
+  StayPermitExpiry: Option(LocalDateTimeFromString),
   Zip: Option(t.string),
   // HRef: t.string
 });
