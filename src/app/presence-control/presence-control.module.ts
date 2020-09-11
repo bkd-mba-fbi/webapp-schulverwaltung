0ee8115976742cdeb454cd542cac7ebb87625127
Claudia Asti <asti@puzzle.ch>
2020-09-11 14:22:18 +0200

    Remove comment component

--- src/app/presence-control/presence-control.module.ts
+++ src/app/presence-control/presence-control.module.ts
@@ -5,7 +5,6 @@ import { PresenceControlRoutingModule } from './presence-control-routing.module'
 import { PresenceControlComponent } from './components/presence-control/presence-control.component';
 import { PresenceControlHeaderComponent } from './components/presence-control-header/presence-control-header.component';
 import { PresenceControlListComponent } from './components/presence-control-list/presence-control-list.component';
-import { PresenceControlCommentComponent } from './components/presence-control-comment/presence-control-comment.component';
 import { PresenceControlEntryComponent } from './components/presence-control-entry/presence-control-entry.component';
 import { PresenceControlDialogComponent } from './components/presence-control-dialog/presence-control-dialog.component';
 import { STUDENT_PROFILE_BACKLINK } from '../shared/tokens/student-profile-backlink';
@@ -16,7 +15,6 @@ import { PresenceControlIncidentComponent } from './components/presence-control-
     PresenceControlComponent,
     PresenceControlHeaderComponent,
     PresenceControlListComponent,
-    PresenceControlCommentComponent,
     PresenceControlEntryComponent,
     PresenceControlDialogComponent,
     PresenceControlIncidentComponent,
