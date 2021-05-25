24589933ee0ec97a32017f44557cdb5b9e3c4029
Claudia Asti <asti@puzzle.ch>
2021-05-25 16:17:44 +0200

    Add lesson teacher rest service

---
+++ src/app/shared/services/lesson-teachers-rest.service.ts
@@ -0,0 +1,27 @@
+import { Inject, Injectable } from '@angular/core';
+import { RestService } from './rest.service';
+import { LessonAbsence } from '../models/lesson-absence.model';
+import { HttpClient } from '@angular/common/http';
+import { Settings, SETTINGS } from '../../settings';
+import { switchMap } from 'rxjs/operators';
+import { decodeArray } from '../utils/decode';
+import { Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class LessonTeachersRestService extends RestService<
+  typeof LessonAbsence
+> {
+  constructor(http: HttpClient, @Inject(SETTINGS) settings: Settings) {
+    super(http, settings, LessonAbsence, 'LessonTeachers');
+  }
+
+  getLessonAbsencesForStudent(
+    studentId: number
+  ): Observable<ReadonlyArray<LessonAbsence>> {
+    return this.http
+      .get<unknown>(
+        `${this.baseUrl}/except/${studentId}/LessonAbsences?expand=LessonRef`
+      )
+      .pipe(switchMap(decodeArray(this.codec)));
+  }
+}
