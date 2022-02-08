46cd78d40a8ddad14036fd9f329e93bdf61c0cce
Claudia Asti <asti@puzzle.ch>
2022-02-08 13:07:22 +0100

    rename settings

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 __webpack_public_path__ =
-  window.absenzenmanagement.settings.scriptsAndAssetsPath + '/';
+  window.schulverwaltung.settings.scriptsAndAssetsPath + '/';
 
 if (environment.production) {
   enableProdMode();
