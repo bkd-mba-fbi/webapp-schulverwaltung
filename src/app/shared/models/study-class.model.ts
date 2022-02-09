2f593e4fef14d9265af272a77084251ad8f2fe99
Claudia Asti <asti@puzzle.ch>
2022-02-09 11:41:55 +0100

    update study class model

--- src/app/shared/models/study-class.model.ts
+++ src/app/shared/models/study-class.model.ts
@@ -2,10 +2,11 @@ import * as t from 'io-ts';
 
 const StudyClass = t.type({
   Id: t.number,
-  // Designation: t.string
-  // DisplayText: t.string,
+  Designation: t.string,
+  StudentCount: t.number,
   Number: t.string,
   // HRef: t.string,
+  // IsActive: t.boolean,
 });
 type StudyClass = t.TypeOf<typeof StudyClass>;
 export { StudyClass };
