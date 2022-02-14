e40c9e97e850dd8983dc2e88bd1094141ff69c78
Marc <fehlmann@puzzle.ch>
2022-02-14 13:28:27 +0100

    Generate module tests refs: 333

---
+++ src/app/tests/tests.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TestsRoutingModule } from './tests-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, TestsRoutingModule],
+})
+export class TestsModule {}
