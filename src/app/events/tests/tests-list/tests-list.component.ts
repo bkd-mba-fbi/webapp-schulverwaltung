6d50a2f840ff6199c7adc042bd18cff231d3147c
Max Burri <burri@puzzle.ch>
2022-02-15 11:27:33 +0100

    get course id as observable from path param, refs 333

--- src/app/events/tests/tests-list/tests-list.component.ts
+++ src/app/events/tests/tests-list/tests-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'erz-tests-list',
@@ -6,5 +9,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tests-list.component.scss'],
 })
 export class TestsListComponent {
-  constructor() {}
+  courseId$: Observable<number> = this.route.paramMap.pipe(
+    map((params) => Number(params.get('id')))
+  );
+
+  constructor(private route: ActivatedRoute) {}
 }
