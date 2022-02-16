23f24052386c3e5fcbcbd3716c8ccb08c2766f68
Max Burri <burri@puzzle.ch>
2022-02-16 13:24:18 +0100

    let the user select a test - and show the test designation, refs 333

--- src/app/events/components/tests-list/tests-list.component.ts
+++ src/app/events/components/tests-list/tests-list.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
-import { map, switchMap } from 'rxjs/operators';
+import { merge, Observable, Subject } from 'rxjs';
+import { distinctUntilChanged, map, switchMap, first } from 'rxjs/operators';
 import { Test } from 'src/app/shared/models/test.model';
 import { TestStateService } from '../../services/test-state.service';
 
@@ -11,14 +11,21 @@ import { TestStateService } from '../../services/test-state.service';
   styleUrls: ['./tests-list.component.scss'],
 })
 export class TestsListComponent {
+  selectTest$: Subject<number> = new Subject();
+
   courseId$: Observable<number> = this.route.paramMap.pipe(
-    map((params) => Number(params.get('id')))
+    map((params) => Number(params.get('id'))),
+    distinctUntilChanged()
   );
 
-  course$ = this.courseId$.pipe(switchMap((id) => this.state.getCourse(id)));
+  course$ = this.courseId$.pipe(
+    switchMap((id) => this.state.getCourse(id)),
+    distinctUntilChanged()
+  );
 
   tests$: Observable<Test[]> = this.course$.pipe(
-    map((course) => course.Tests ?? [])
+    map((course) => course.Tests ?? []),
+    distinctUntilChanged()
   );
 
   testOptions$ = this.tests$.pipe(
@@ -26,11 +33,25 @@ export class TestsListComponent {
       test.map((test) => {
         return { Key: test.Id, Value: test.Designation };
       })
-    )
+    ),
+    distinctUntilChanged()
   );
 
-  // TODO: do I need to check wether the array of tests is empty?
-  selectedTestId$ = this.tests$.pipe(map((tests) => tests[0]?.Id));
+  selectedTestId$ = merge(
+    this.selectTest$,
+    this.tests$.pipe(map((tests) => tests[0]?.Id))
+  ).pipe(distinctUntilChanged());
+
+  selectedTest$ = this.selectedTestId$.pipe(
+    switchMap((id) =>
+      this.tests$.pipe(map((tests) => tests.find((test) => test.Id === id)))
+    ),
+    distinctUntilChanged()
+  );
 
   constructor(public state: TestStateService, private route: ActivatedRoute) {}
+
+  testSelected(id: number) {
+    this.selectTest$.next(id);
+  }
 }
