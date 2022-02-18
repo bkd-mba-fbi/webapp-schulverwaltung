b9dbe2e911eb742480d1fd6bbc8fc5459ef6a329
Max Burri <burri@puzzle.ch>
2022-02-18 10:46:02 +0100

    use correct setup for MySettingsShowComponent

--- src/app/my-settings/components/my-settings-show/my-settings-show.component.spec.ts
+++ src/app/my-settings/components/my-settings-show/my-settings-show.component.spec.ts
@@ -1,4 +1,7 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { buildTestModuleMetadata } from 'src/spec-helpers';
+import { MySettingsService } from '../../services/my-settings.service';
+import { MySettingsNotificationsComponent } from '../my-settings-notifications/my-settings-notifications.component';
 
 import { MySettingsShowComponent } from './my-settings-show.component';
 
@@ -6,11 +9,19 @@ describe('MySettingsShowComponent', () => {
   let component: MySettingsShowComponent;
   let fixture: ComponentFixture<MySettingsShowComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [MySettingsShowComponent],
-    }).compileComponents();
-  });
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule(
+        buildTestModuleMetadata({
+          providers: [MySettingsService],
+          declarations: [
+            MySettingsShowComponent,
+            MySettingsNotificationsComponent,
+          ],
+        })
+      ).compileComponents();
+    })
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(MySettingsShowComponent);
