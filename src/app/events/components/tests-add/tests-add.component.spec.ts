0a53f3416c124429c348a02633d1ea502753b5fa
Claudia Asti <asti@puzzle.ch>
2022-02-24 11:55:36 +0100

    show grading scale in header, refs 334

--- src/app/events/components/tests-add/tests-add.component.spec.ts
+++ src/app/events/components/tests-add/tests-add.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { buildTestModuleMetadata } from 'src/spec-helpers';
+import { TestStateService } from '../../services/test-state.service';
 
 import { TestsAddComponent } from './tests-add.component';
 
@@ -11,6 +12,7 @@ describe('TestsAddComponent', () => {
     await TestBed.configureTestingModule(
       buildTestModuleMetadata({
         declarations: [TestsAddComponent],
+        providers: [TestStateService],
       })
     ).compileComponents();
   });
