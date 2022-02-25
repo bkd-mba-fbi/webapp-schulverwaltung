95bd9af6c6918d8bd54fb4e57d9816b343261489
Claudia Asti <asti@puzzle.ch>
2022-02-25 11:07:33 +0100

    format entry data according to mockup, refs 334

--- src/app/events/components/tests-add-entry/tests-add-entry.component.ts
+++ src/app/events/components/tests-add-entry/tests-add-entry.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 import { Test } from 'src/app/shared/models/test.model';
 
 @Component({
@@ -9,5 +10,23 @@ import { Test } from 'src/app/shared/models/test.model';
 export class TestsAddEntryComponent {
   @Input() test: Test;
 
-  constructor() {}
+  constructor(private translate: TranslateService) {}
+
+  get gradeType(): string {
+    const points = this.test.MaxPointsAdjusted
+      ? `${this.test.MaxPointsAdjusted}, ${this.translate.instant(
+          'tests.add.adjusted'
+        )}`
+      : this.test.MaxPoints;
+
+    return this.test.IsPointGrading
+      ? `${this.translate.instant('tests.add.points')} (${points})`
+      : this.translate.instant('tests.add.grades');
+  }
+
+  get factor(): string {
+    return `${this.translate.instant('tests.add.factor')} ${
+      this.test.Weight
+    } (${this.test.WeightPercent}%)`;
+  }
 }
