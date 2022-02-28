3a11e79a25a065d40fdd0f0ae8094fbb37799545
Max Burri <burri@puzzle.ch>
2022-02-28 15:28:13 +0100

    pass the selected test as input to the edit grades component, refs 333

--- src/app/events/components/test-edit-grades/test-edit-grades.component.ts
+++ src/app/events/components/test-edit-grades/test-edit-grades.component.ts
@@ -14,6 +14,7 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class TestEditGradesComponent implements OnInit {
   @Input() course: Course;
+  @Input() selectedTest: Test;
 
   studentGrades: StudentGrade[];
   displayTests: any;
