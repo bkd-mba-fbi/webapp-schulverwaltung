a8681db396ab82b6870d41bbc45e57336753d0a5
Claudia Asti <asti@puzzle.ch>
2022-03-01 12:03:37 +0100

    add pipes for test weight and grading type, refs 333

---
+++ src/app/shared/pipes/test-weight.pipe.spec.ts
@@ -0,0 +1,33 @@
+import { TestBed, waitForAsync } from '@angular/core/testing';
+import { TranslateService } from '@ngx-translate/core';
+import { Test } from 'src/app/shared/models/test.model';
+import { buildTest } from 'src/spec-builders';
+import { buildTestModuleMetadata } from 'src/spec-helpers';
+import { TestsWeightPipe } from './test-weight.pipe';
+
+describe('TestsWeightPipe', () => {
+  let pipe: TestsWeightPipe;
+  let test: Test;
+
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule(
+        buildTestModuleMetadata({})
+      ).compileComponents();
+      pipe = new TestsWeightPipe(TestBed.inject(TranslateService));
+    })
+  );
+
+  beforeEach(() => {
+    test = buildTest(1, 2, []);
+  });
+
+  it('should return the formatted weight', () => {
+    test.Weight = 50;
+    test.WeightPercent = 12.5;
+
+    const result = pipe.transform(test);
+
+    expect(result).toBe('tests.factor 50 (12.5%)');
+  });
+});
