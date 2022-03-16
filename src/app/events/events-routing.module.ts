907448e3ef6faab79a600762a4298522a45a25bd
Claudia Asti <asti@puzzle.ch>
2022-03-16 11:50:25 +0100

    add basic routing to edit form, remove obsolete add page, refs 335

--- src/app/events/events-routing.module.ts
+++ src/app/events/events-routing.module.ts
@@ -4,8 +4,8 @@ import { EventsComponent } from './components/events/events.component';
 import { TestsComponent } from './components/tests/tests.component';
 import { TestsListComponent } from './components/tests-list/tests-list.component';
 import { EventsListComponent } from './components/events-list/events-list.component';
-import { TestsAddComponent } from './components/tests-add/tests-add.component';
 import { TestsEditComponent } from './components/tests-edit/tests-edit.component';
+import { TestsAddComponent } from './components/tests-add/tests-add.component';
 
 const routes: Routes = [
   {
@@ -31,7 +31,7 @@ const routes: Routes = [
         component: TestsAddComponent,
       },
       {
-        path: ':id/tests/edit',
+        path: ':id/tests/edit/:testId',
         component: TestsEditComponent,
       },
     ],
