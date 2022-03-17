fa93066deddfad707ae84ae4b9d73fba457842b2
Claudia Asti <asti@puzzle.ch>
2022-03-17 15:41:32 +0100

    implement form layout, refs 335

--- src/app/events/components/tests-edit-form/tests-edit-form.component.ts
+++ src/app/events/components/tests-edit-form/tests-edit-form.component.ts
@@ -1,4 +1,9 @@
+import { formatPercent, PercentPipe } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { of } from 'rxjs';
+import { Test } from 'src/app/shared/models/test.model';
+import { I18nService } from 'src/app/shared/services/i18n.service';
 
 @Component({
   selector: 'erz-tests-edit-form',
@@ -7,6 +12,21 @@ import { Component, Input } from '@angular/core';
 })
 export class TestsEditFormComponent {
   @Input() courseId: Number;
+  @Input() test: Option<Test>;
 
-  constructor() {}
+  formGroup$ = of(this.createFormGroup());
+
+  constructor(private fb: FormBuilder) {}
+
+  private createFormGroup(): FormGroup {
+    return this.fb.group({
+      designation: ['', Validators.required],
+      date: [null, Validators.required],
+      factor: [1, Validators.required],
+      percentage: [1],
+      isPointGrading: [false, Validators.required],
+      maxPoints: [null],
+      maxPointsAdjusted: [null],
+    });
+  }
 }
