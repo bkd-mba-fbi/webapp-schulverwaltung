c0816bc3d3bd5545fab2b8ddb812cd2ac5372111
Claudia Asti <asti@puzzle.ch>
2022-03-23 11:17:50 +0100

    submit add test request, refs 335

--- src/app/events/components/tests-edit-form/tests-edit-form.component.ts
+++ src/app/events/components/tests-edit-form/tests-edit-form.component.ts
@@ -1,6 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { ToastrService } from 'ngx-toastr';
+import { BehaviorSubject, of } from 'rxjs';
 import { Test } from 'src/app/shared/models/test.model';
+import { CoursesRestService } from 'src/app/shared/services/courses-rest.service';
+import { getValidationErrors } from 'src/app/shared/utils/form';
 
 @Component({
   selector: 'erz-tests-edit-form',
@@ -8,37 +14,103 @@ import { Test } from 'src/app/shared/models/test.model';
   styleUrls: ['./tests-edit-form.component.scss'],
 })
 export class TestsEditFormComponent implements OnInit {
-  @Input() courseId: Number;
+  @Input() courseId: number;
   @Input() test: Option<Test>;
 
-  formGroup: FormGroup;
+  formGroup: FormGroup = this.createFormGroup();
+  private submitted$ = new BehaviorSubject(false);
+  saving$ = new BehaviorSubject(false);
 
-  constructor(private fb: FormBuilder) {}
+  designationErrors$ = getValidationErrors(
+    of(this.formGroup),
+    this.submitted$,
+    'designation'
+  );
+
+  dateErrors$ = getValidationErrors(
+    of(this.formGroup),
+    this.submitted$,
+    'date'
+  );
+
+  maxPointsErrors$ = getValidationErrors(
+    of(this.formGroup),
+    this.submitted$,
+    'maxPoints'
+  );
+
+  constructor(
+    private fb: FormBuilder,
+    private courseService: CoursesRestService,
+    private toastr: ToastrService,
+    private translate: TranslateService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-    this.formGroup = this.createFormGroup(this.test);
+    if (this.test) {
+      this.setInitialValues(this.test);
+    }
   }
 
-  private createFormGroup(test: Option<Test>): FormGroup {
-    let fg = this.fb.group({
-      Designation: ['', Validators.required],
-      Date: [null, Validators.required],
-      Weight: [1, Validators.required],
-      WeightPercent: [1],
-      IsPointGrading: [false, Validators.required],
-      MaxPoints: [null],
-      MaxPointsAdjusted: [null],
-    });
-
-    if (test) {
-      fg.patchValue(test);
-      fg.controls.Date.setValue({
-        day: test.Date.getDate(),
-        month: test.Date.getMonth() + 1,
-        year: test.Date.getFullYear(),
-      });
+  // TODO delegate to add component
+  onSubmit(): void {
+    this.submitted$.next(true);
+    if (this.formGroup.valid) {
+      this.save(this.formGroup);
     }
+  }
 
-    return fg;
+  private save(formGroup: FormGroup): void {
+    this.saving$.next(true);
+    const {
+      designation,
+      date,
+      weight,
+      isPointGrading,
+      maxPoints,
+      maxPointsAdjusted,
+    } = formGroup.value;
+    this.courseService
+      .add(
+        this.courseId,
+        new Date(date),
+        designation,
+        weight,
+        isPointGrading,
+        maxPoints,
+        maxPointsAdjusted
+      )
+      .subscribe(this.onSaveSuccess.bind(this));
+  }
+
+  private onSaveSuccess(): void {
+    this.toastr.success(this.translate.instant('tests.form.save-success'));
+    this.navigateBack();
+  }
+
+  private navigateBack(): void {
+    this.router.navigate(['events', this.courseId, 'tests']);
+  }
+
+  private createFormGroup(): FormGroup {
+    return this.fb.group({
+      designation: ['', Validators.required],
+      date: [null], // TODO required, enable validation for date select component
+      weight: [1],
+      weightPercent: [1],
+      isPointGrading: [false],
+      maxPoints: [null], // TODO required if is point grading
+      maxPointsAdjusted: [null],
+    });
+  }
+
+  private setInitialValues(test: Test) {
+    this.formGroup.patchValue(test);
+    this.formGroup.controls.date.setValue({
+      day: test.Date.getDate(),
+      month: test.Date.getMonth() + 1,
+      year: test.Date.getFullYear(),
+    });
   }
 }
