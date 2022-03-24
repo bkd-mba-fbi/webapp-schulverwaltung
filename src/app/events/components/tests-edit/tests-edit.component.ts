4e8a1956ab482e541e9b135828f75bd096fcd0bb
Claudia Asti <asti@puzzle.ch>
2022-03-24 18:14:46 +0100

    prevent test deletion and type change if grades are present, refs 335

--- src/app/events/components/tests-edit/tests-edit.component.ts
+++ src/app/events/components/tests-edit/tests-edit.component.ts
@@ -50,6 +50,11 @@ export class TestsEditComponent {
   ) {}
 
   delete(test: Test): void {
+    if (test.Results && test.Results.length > 0) {
+      alert(this.translate.instant('tests.form.delete-not-allowed'));
+      return;
+    }
+
     if (confirm(this.translate.instant('tests.form.confirm'))) {
       this.courseService
         .delete(test.CourseId, test.Id)
