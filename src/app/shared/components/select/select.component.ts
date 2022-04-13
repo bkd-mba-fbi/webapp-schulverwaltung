f36f800e60dc8ccf38a07e844c08cd9a82091c06
Marc <fehlmann@puzzle.ch>
2022-04-13 14:44:52 +0200

    Rewrite erz-select component to use ng-select, refs: 333

--- src/app/shared/components/select/select.component.ts
+++ src/app/shared/components/select/select.component.ts
@@ -1,46 +1,20 @@
-import {
-  Component,
-  OnInit,
-  Input,
-  Output,
-  EventEmitter,
-  OnChanges,
-  SimpleChanges,
-} from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 import { DropDownItem } from '../../models/drop-down-item.model';
-import { BehaviorSubject, combineLatest } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'erz-select',
   templateUrl: './select.component.html',
   styleUrls: ['./select.component.scss'],
 })
-export class SelectComponent implements OnChanges {
+export class SelectComponent {
   @Input() options: ReadonlyArray<DropDownItem> = [];
-  @Input() allowEmpty = true;
   @Input() value: Option<number> = null;
   @Output() valueChange = new EventEmitter<Option<number>>();
 
-  options$ = new BehaviorSubject<ReadonlyArray<DropDownItem>>([]);
-  rawValue$ = new BehaviorSubject<Option<number>>(null);
-
-  value$ = combineLatest([this.rawValue$, this.options$]).pipe(
-    map(
-      ([rawValue, options]) =>
-        (options && options.find((o) => o.Key === rawValue)) || null
-    )
-  );
-
   constructor() {}
 
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes.value) {
-      this.rawValue$.next(changes.value.currentValue);
-    }
-    if (changes.options) {
-      this.options$.next(changes.options.currentValue);
-    }
+  itemChanged(): void {
+    this.valueChange.emit(this.value);
   }
 }
