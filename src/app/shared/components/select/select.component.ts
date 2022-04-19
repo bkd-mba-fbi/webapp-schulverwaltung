23f67611c523f37cc973533b31d0f594c8cdbeef
Claudia Asti <asti@puzzle.ch>
2022-04-19 13:50:07 +0200

    Revert "Rewrite erz-select component to use ng-select, refs: 333"
    This reverts commit f36f800e60dc8ccf38a07e844c08cd9a82091c06.

--- src/app/shared/components/select/select.component.ts
+++ src/app/shared/components/select/select.component.ts
@@ -1,20 +1,46 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
 
 import { DropDownItem } from '../../models/drop-down-item.model';
+import { BehaviorSubject, combineLatest } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'erz-select',
   templateUrl: './select.component.html',
   styleUrls: ['./select.component.scss'],
 })
-export class SelectComponent {
+export class SelectComponent implements OnChanges {
   @Input() options: ReadonlyArray<DropDownItem> = [];
+  @Input() allowEmpty = true;
   @Input() value: Option<number> = null;
   @Output() valueChange = new EventEmitter<Option<number>>();
 
+  options$ = new BehaviorSubject<ReadonlyArray<DropDownItem>>([]);
+  rawValue$ = new BehaviorSubject<Option<number>>(null);
+
+  value$ = combineLatest([this.rawValue$, this.options$]).pipe(
+    map(
+      ([rawValue, options]) =>
+        (options && options.find((o) => o.Key === rawValue)) || null
+    )
+  );
+
   constructor() {}
 
-  itemChanged(): void {
-    this.valueChange.emit(this.value);
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.value) {
+      this.rawValue$.next(changes.value.currentValue);
+    }
+    if (changes.options) {
+      this.options$.next(changes.options.currentValue);
+    }
   }
 }
