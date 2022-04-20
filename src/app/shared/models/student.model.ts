3aa01ad85149eda0dec7e82976193f971a980feb
Max Burri <burri@puzzle.ch>
2022-04-20 12:03:35 +0200

    make properties for students optional according to changes in backend

--- src/app/shared/models/student.model.ts
+++ src/app/shared/models/student.model.ts
@@ -3,18 +3,18 @@ import { Maybe, Option, LocalDateTimeFromString } from './common-types';
 
 const Student = t.type({
   Id: t.number,
-  AddressLine1: t.string,
+  AddressLine1: Option(t.string),
   AddressLine2: Maybe(t.string),
-  Birthdate: LocalDateTimeFromString,
+  Birthdate: Option(LocalDateTimeFromString),
   DisplayEmail: Option(t.string),
   // FirstName: t.string,
   FullName: t.string,
   Gender: t.union([t.literal('M'), t.literal('F'), t.literal('X')]),
   // LastName: t.string,
-  Location: t.string,
+  Location: Option(t.string),
   PhoneMobile: Option(t.string),
   PhonePrivate: Option(t.string),
-  PostalCode: t.string,
+  PostalCode: Option(t.string),
   // HRef: t.string
 });
 type Student = t.TypeOf<typeof Student>;
