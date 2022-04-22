395e6737cf787cd45abfa954e64da9a4bb73a988
Max Burri <burri@puzzle.ch>
2022-04-22 10:18:08 +0200

    improve existing tests for grade component, refs 336

--- src/app/events/components/grade/grade.component.spec.ts
+++ src/app/events/components/grade/grade.component.spec.ts
@@ -7,12 +7,19 @@ import {
   buildStudent,
   buildTest,
 } from '../../../../spec-builders';
-import { GradeOrNoResult } from 'src/app/shared/models/student-grades';
+import {
+  Grade,
+  GradeOrNoResult,
+  NoResult,
+} from 'src/app/shared/models/student-grades';
 import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { byTestId } from 'src/specs/spec-utils';
 
 describe('GradeComponent', () => {
   let component: GradeComponent;
   let fixture: ComponentFixture<GradeComponent>;
+  let debugElement: DebugElement;
 
   const result = buildResult(120, 140);
   const test = buildTest(100, 120, [buildResult(120, 140)]);
@@ -32,90 +39,109 @@ describe('GradeComponent', () => {
     fixture = TestBed.createComponent(GradeComponent);
     component = fixture.componentInstance;
     component.student = student;
+    debugElement = fixture.debugElement;
   });
 
   it('should create', () => {
+    // given
     component.grade = {
       kind: 'grade',
       result,
       test,
     };
+    // when
     fixture.detectChanges();
+
+    // then
     expect(component).toBeTruthy();
   });
 
-  it('should create with noResult', () => {
-    const noResult: GradeOrNoResult = {
-      kind: 'no-result',
-      test,
-    };
+  describe('should show values (points and grade)', () => {
+    it('should create with noResult', () => {
+      // given
+      const noResult: NoResult = {
+        kind: 'no-result',
+        test,
+      };
 
-    component.grade = noResult;
+      noResult.test.IsPointGrading = true;
+      component.grade = noResult;
 
-    expect(component).toBeTruthy();
-    expect(component.grade).toBe(noResult);
+      // when
+      fixture.detectChanges();
+
+      expectPointsInputValue(debugElement, '');
+    });
+
+    it('should show points', () => {
+      // given
+      const grade: Grade = {
+        kind: 'grade',
+        result,
+        test,
+      };
+
+      grade.test.IsPointGrading = true;
+      grade.result.Points = 11;
+
+      // when
+      component.grade = grade;
+      fixture.detectChanges();
+
+      // then
+      expectPointsInputValue(debugElement, '11');
+    });
   });
+  describe('points input validation', () => {
+    let grade: Grade;
 
-  it('should show points', () => {
-    const grade: GradeOrNoResult = {
-      kind: 'grade',
-      result,
-      test,
-    };
+    beforeEach(() => {
+      grade = {
+        kind: 'grade',
+        result,
+        test,
+      };
 
-    grade.test.IsPointGrading = true;
-    grade.result.Points = 11;
+      grade.test.IsPointGrading = true;
+      grade.result.Points = 12;
+    });
 
-    component.grade = grade;
-    fixture.detectChanges();
+    it('should show validation error if points are greater than maxPointsAdjusted', () => {
+      // given
+      grade.test.MaxPoints = 13;
+      grade.test.MaxPointsAdjusted = 11;
+      component.grade = grade;
 
-    const input = fixture.debugElement.query(By.css('.point-input'))
-      .nativeElement;
+      // when
+      fixture.detectChanges();
 
-    expect(input.value).toContain(11);
-  });
+      // then
+      expectValidationErrorMessage(debugElement);
+    });
 
-  it('should show validation error if points are greater than maxPointsAdjusted', () => {
-    const grade: GradeOrNoResult = {
-      kind: 'grade',
-      result,
-      test,
-    };
+    it('should show validation error if points are greater than maxPoints', () => {
+      // given
+      grade.test.MaxPoints = 11;
+      grade.test.MaxPointsAdjusted = null;
+      component.grade = grade;
 
-    grade.test.IsPointGrading = true;
-    grade.test.MaxPoints = 13;
-    grade.test.MaxPointsAdjusted = 11;
-    grade.result.Points = 12;
+      // when
+      fixture.detectChanges();
 
-    component.grade = grade;
-    fixture.detectChanges();
-
-    const error = fixture.nativeElement.querySelector('.invalid-feedback');
-
-    expect(error.textContent).toContain(
-      'global.validation-errors.invalidPoints'
-    );
-  });
-
-  it('should show validation error if points are greater than maxPoints', () => {
-    const grade: GradeOrNoResult = {
-      kind: 'grade',
-      result,
-      test,
-    };
-
-    grade.test.IsPointGrading = true;
-    grade.test.MaxPoints = 11;
-    grade.test.MaxPointsAdjusted = null;
-    grade.result.Points = 12;
-
-    component.grade = grade;
-    fixture.detectChanges();
-
-    const error = fixture.nativeElement.querySelector('.invalid-feedback');
-
-    expect(error.textContent).toContain(
-      'global.validation-errors.invalidPoints'
-    );
+      // then
+      expectValidationErrorMessage(debugElement);
+    });
   });
 });
+
+function expectPointsInputValue(debugElement: DebugElement, expected: string) {
+  const input = debugElement.query(byTestId('point-input')).nativeElement;
+  expect(input.value).toBe(expected);
+}
+
+function expectValidationErrorMessage(debugElement: DebugElement) {
+  const error = debugElement.query(byTestId('validation-error-message'))
+    .nativeElement;
+
+  expect(error.textContent).toContain('global.validation-errors.invalidPoints');
+}
