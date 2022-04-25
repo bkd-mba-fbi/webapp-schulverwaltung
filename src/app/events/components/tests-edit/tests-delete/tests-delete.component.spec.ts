bae0af05245edb48f0186b345e571ba77230fc38
Marc <fehlmann@puzzle.ch>
2022-04-25 13:33:08 +0200

    Add tests for test delete modal, refs 335

--- src/app/events/components/tests-edit/tests-delete/tests-delete.component.spec.ts
+++ src/app/events/components/tests-edit/tests-delete/tests-delete.component.spec.ts
@@ -3,11 +3,14 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TestsDeleteComponent } from './tests-delete.component';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { buildTestModuleMetadata } from '../../../../../spec-helpers';
-import { buildTest } from 'src/spec-builders';
+import { buildResult, buildTest } from 'src/spec-builders';
+import { byTestId } from '../../../../../specs/spec-utils';
+import { DebugElement } from '@angular/core';
 
 describe('TestsDeleteComponent', () => {
   let component: TestsDeleteComponent;
   let fixture: ComponentFixture<TestsDeleteComponent>;
+  let debugElement: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule(
@@ -21,10 +24,55 @@ describe('TestsDeleteComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TestsDeleteComponent);
     component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create with no results', () => {
+    component.test = buildTest(100, 10, []);
+
+    fixture.detectChanges();
+
+    expect(
+      debugElement
+        .query(byTestId('confirmation-message'))
+        .nativeElement.textContent.trim()
+    ).toBe('tests.form.confirm');
+
+    expect(
+      debugElement
+        .query(byTestId('cancel-button'))
+        .nativeElement.textContent.trim()
+    ).toBe('tests.dialog.no');
+
+    expect(
+      debugElement
+        .query(byTestId('confirm-button'))
+        .nativeElement.textContent.trim()
+    ).toBe('tests.dialog.yes');
+
+    expect(component.canDeleteTest).toBeTrue();
+    expect(component).toBeTruthy();
+  });
+
+  it('should create with result', () => {
+    component.test = buildTest(100, 10, [buildResult(10, 20)]);
+
+    fixture.detectChanges();
+
+    expect(
+      debugElement
+        .query(byTestId('confirmation-message'))
+        .nativeElement.textContent.trim()
+    ).toBe('tests.form.delete-not-allowed');
+
+    expect(
+      debugElement
+        .query(byTestId('confirm-button'))
+        .nativeElement.textContent.trim()
+    ).toBe('tests.dialog.ok');
+
+    expect(component.canDeleteTest).toBeFalse();
     expect(component).toBeTruthy();
   });
 });
