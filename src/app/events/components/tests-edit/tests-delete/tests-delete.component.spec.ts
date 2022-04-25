1100c7856b7fed1854b506da35efbc1808c07f92
Claudia Asti <asti@puzzle.ch>
2022-04-25 12:15:59 +0200

    move can delete check  to test modal, refs 335

--- src/app/events/components/tests-edit/tests-delete/tests-delete.component.spec.ts
+++ src/app/events/components/tests-edit/tests-delete/tests-delete.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TestsDeleteComponent } from './tests-delete.component';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { buildTestModuleMetadata } from '../../../../../spec-helpers';
+import { buildTest } from 'src/spec-builders';
 
 describe('TestsDeleteComponent', () => {
   let component: TestsDeleteComponent;
