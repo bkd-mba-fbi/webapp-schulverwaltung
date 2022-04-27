c1f4586765151e164cee77d84a8708ed9241c889
Max Burri <burri@puzzle.ch>
2022-04-27 08:46:01 +0200

    add id of finalGrade as input, refs 338

--- src/app/events/components/grades/grade-select/grade-select.component.ts
+++ src/app/events/components/grades/grade-select/grade-select.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { number } from 'io-ts';
 import { DropDownItem } from 'src/app/shared/models/drop-down-item.model';
+import { FinalGrade } from 'src/app/shared/models/student-grades';
 
 @Component({
   selector: 'erz-grade-select',
@@ -8,13 +10,18 @@ import { DropDownItem } from 'src/app/shared/models/drop-down-item.model';
 })
 export class GradeSelectComponent {
   @Input() options: DropDownItem[];
-  @Input() gradeId: Option<number>;
+  @Input() selectedGradeId: Option<number>; // the selected grade - id from gradingscale
+  @Input() gradeId: Option<number>; // the id of the grade itself
 
-  @Output() selectedGradeId = new EventEmitter<number>();
+  @Output() gradeIdSelected = new EventEmitter<{
+    id: number;
+    selectedGradeId: number;
+  }>();
 
   constructor() {}
 
-  onGradeChange(gradeId: number): void {
-    this.selectedGradeId.emit(gradeId);
+  onGradeChange(selectedGradeId: number): void {
+    if (this.gradeId?.valueOf() === undefined) return;
+    this.gradeIdSelected.emit({ id: this.gradeId?.valueOf(), selectedGradeId });
   }
 }
