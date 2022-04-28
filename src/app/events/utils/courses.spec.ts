120ef212bdc0df8bc4cdfd83a0a6bff702222885
Marc <fehlmann@puzzle.ch>
2022-04-28 11:02:38 +0200

    Add tests to verify assignment of state to course, refs 338

---
+++ src/app/events/utils/courses.spec.ts
@@ -0,0 +1,99 @@
+import { CourseState, getState } from './courses';
+import { buildCourse } from '../../../spec-builders';
+
+describe('Course utils', () => {
+  describe('Get course state', () => {
+    beforeEach(() => {
+      jasmine.clock().install();
+      jasmine.clock().mockDate(new Date(2022, 1, 3));
+    });
+
+    afterEach(() => {
+      jasmine.clock().uninstall();
+    });
+
+    it('should get no state', () => {
+      // given
+      const evaluationStatusRef = {
+        HasEvaluationStarted: false,
+        EvaluationUntil: null,
+        HasReviewOfEvaluationStarted: false,
+        HasTestGrading: false,
+        Id: 6980,
+      };
+
+      const course = buildCourse(
+        1234,
+        'Course in no state',
+        undefined,
+        evaluationStatusRef
+      );
+
+      // then
+      expect(getState(course)).toEqual(null);
+    });
+
+    it('should get state add-tests', () => {
+      // given
+      const evaluationStatusRef = {
+        HasEvaluationStarted: false,
+        EvaluationUntil: null,
+        HasReviewOfEvaluationStarted: false,
+        HasTestGrading: true,
+        Id: 6980,
+      };
+
+      const course = buildCourse(
+        1234,
+        'Course in state add-tests',
+        undefined,
+        evaluationStatusRef
+      );
+
+      // then
+      expect(getState(course)).toEqual(CourseState.Tests);
+    });
+
+    it('should get state rating-until', () => {
+      // given
+      const evaluationStatusRef = {
+        HasEvaluationStarted: true,
+        EvaluationUntil: new Date(2022, 2, 3),
+        HasReviewOfEvaluationStarted: false,
+        HasTestGrading: false,
+        Id: 6980,
+      };
+
+      const course = buildCourse(
+        1234,
+        'Course in state rating-until',
+        undefined,
+        evaluationStatusRef
+      );
+
+      // then
+      expect(getState(course)).toEqual(CourseState.RatingUntil);
+    });
+
+    it('should get state intermediate-rating', () => {
+      // given
+      const evaluationStatusRef = {
+        HasEvaluationStarted: true,
+        EvaluationUntil: null,
+        HasReviewOfEvaluationStarted: false,
+        HasTestGrading: false,
+        Id: 6980,
+      };
+
+      const course = buildCourse(
+        1234,
+        'Course in state intermediate-rating',
+        undefined,
+        evaluationStatusRef
+      );
+
+      // then
+      expect(getState(course)).toEqual(CourseState.IntermediateRating);
+    });
+  });
+});
