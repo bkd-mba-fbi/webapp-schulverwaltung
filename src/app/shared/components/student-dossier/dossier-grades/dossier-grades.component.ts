c6e56ace156b56accd4d7ddda33c91b0d138f95d
Max Burri <burri@puzzle.ch>
2022-05-02 14:26:02 +0200

    load overview of tests/ grades for a given student, #337

--- src/app/shared/components/student-dossier/dossier-grades/dossier-grades.component.ts
+++ src/app/shared/components/student-dossier/dossier-grades/dossier-grades.component.ts
@@ -1,14 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'erz-dossier-grades',
   templateUrl: './dossier-grades.component.html',
   styleUrls: ['./dossier-grades.component.scss'],
 })
-export class DossierGradesComponent implements OnInit {
+export class DossierGradesComponent {
   constructor() {}
-
-  ngOnInit(): void {
-    console.log('dossier-grades works!');
-  }
 }
