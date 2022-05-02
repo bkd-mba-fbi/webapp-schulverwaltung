0ed6f31509c9e78df81cb8a2f92220996618fdc1
Max Burri <burri@puzzle.ch>
2022-05-02 14:44:35 +0200

    configure testbed, refs #337

--- src/app/shared/components/student-dossier/dossier-grades/dossier-grades.component.spec.ts
+++ src/app/shared/components/student-dossier/dossier-grades/dossier-grades.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { buildTestModuleMetadata } from 'src/spec-helpers';
 
 import { DossierGradesComponent } from './dossier-grades.component';
 
@@ -7,9 +8,11 @@ describe('StudentGradesComponent', () => {
   let fixture: ComponentFixture<DossierGradesComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [DossierGradesComponent],
-    }).compileComponents();
+    await TestBed.configureTestingModule(
+      buildTestModuleMetadata({
+        declarations: [DossierGradesComponent],
+      })
+    ).compileComponents();
   });
 
   beforeEach(() => {
