b9452c9e33259354e7cf8c020b4d83fe8a710a2d
Max Burri <burri@puzzle.ch>
2022-05-02 14:45:14 +0200

    get studentid from dossier service, #337

--- src/app/shared/components/student-dossier/dossier-grades/dossier-grades.component.ts
+++ src/app/shared/components/student-dossier/dossier-grades/dossier-grades.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {
+  distinctUntilChanged,
+  Observable,
+  Subject,
+  takeUntil,
+  tap,
+} from 'rxjs';
+import { DossierStateService } from 'src/app/shared/services/dossier-state.service';
 
 @Component({
   selector: 'erz-dossier-grades',
   templateUrl: './dossier-grades.component.html',
   styleUrls: ['./dossier-grades.component.scss'],
 })
-export class DossierGradesComponent {
-  constructor() {}
+export class DossierGradesComponent implements OnInit, OnDestroy {
+  constructor(public state: DossierStateService) {}
+
+  private destroy$ = new Subject<void>();
+
+  ngOnInit(): void {
+    this.state.studentId$.pipe(takeUntil(this.destroy$)).subscribe(console.log);
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+  }
 }
