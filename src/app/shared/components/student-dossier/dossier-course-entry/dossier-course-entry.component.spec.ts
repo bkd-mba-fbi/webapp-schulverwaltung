c6a1d65c79d01ede8a868f0b3b5017e63a42cca6
Max Burri <burri@puzzle.ch>
2022-05-03 15:07:20 +0200

    fix and test message if course has no tests, #337

--- src/app/shared/components/student-dossier/dossier-course-entry/dossier-course-entry.component.spec.ts
+++ src/app/shared/components/student-dossier/dossier-course-entry/dossier-course-entry.component.spec.ts
@@ -1,11 +1,15 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Course } from 'src/app/shared/models/course.model';
 import { buildCourse } from 'src/spec-builders';
 import { buildTestModuleMetadata } from 'src/spec-helpers';
+import { expectText } from 'src/specs/expectations';
 import { DossierCourseEntryComponent } from './dossier-course-entry.component';
 
 describe('DossierCourseEntryComponent', () => {
   let component: DossierCourseEntryComponent;
   let fixture: ComponentFixture<DossierCourseEntryComponent>;
+  let debugElement: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule(
@@ -20,9 +24,17 @@ describe('DossierCourseEntryComponent', () => {
     component = fixture.componentInstance;
     component.course = buildCourse(1234);
     fixture.detectChanges();
+    debugElement = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show message course has no tests', () => {
+    const course: Course = ({ Tests: [] } as unknown) as Course;
+    component.course = course;
+    fixture.detectChanges();
+    expectText(debugElement, 'message-no-tests', 'dossier.no-tests');
   });
 });
