6dd4a0316541a5498f6244d25de37b0c1b733d85
Max Burri <burri@puzzle.ch>
2022-05-05 14:42:04 +0200

    improve error message

--- src/specs/expectations.ts
+++ src/specs/expectations.ts
@@ -6,8 +6,9 @@ export function expectText(
   testId: string,
   expected: string
 ) {
-  const element = debugElement.query(byTestId(testId));
+  assertDebugElement(debugElement);
 
+  const element = debugElement.query(byTestId(testId));
   if (!element) {
     console.error(debugElement.nativeElement.innerHTML);
     throw new Error(
@@ -22,6 +23,7 @@ export function expectNotInTheDocument(
   debugElement: DebugElement,
   testId: string
 ) {
+  assertDebugElement(debugElement);
   expect(debugElement.query(byTestId(testId))).toBeNull();
 }
 
@@ -29,5 +31,17 @@ export function expectElementPresent(
   debugElement: DebugElement,
   testId: string
 ) {
+  assertDebugElement(debugElement);
   expect(debugElement.query(byTestId(testId))).toBeTruthy();
+}
+
+function assertDebugElement(debugElement: DebugElement) {
+  if (!debugElement)
+    throw new Error(`The debugElement you gave me is not defined!
+    You probably forgot to set it up in your beforeEach clause like this:
+
+    debugElement = fixture.debugElement
+
+    Please fix it and rerun your tests.
+  `);
 }
