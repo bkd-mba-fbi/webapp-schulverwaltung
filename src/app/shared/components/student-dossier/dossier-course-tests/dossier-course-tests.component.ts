c3075546739dec005cdaa0d734e59f13813f8fa3
Claudia Asti <asti@puzzle.ch>
2022-05-06 15:13:30 +0200

    move functions from specific component to dossier grade service, refs #337

--- src/app/shared/components/student-dossier/dossier-course-tests/dossier-course-tests.component.ts
+++ src/app/shared/components/student-dossier/dossier-course-tests/dossier-course-tests.component.ts
@@ -1,11 +1,8 @@
 import { Component, Input } from '@angular/core';
-import {
-  Course,
-  FinalGrading,
-  Grading,
-} from 'src/app/shared/models/course.model';
+import { Course, Grading } from 'src/app/shared/models/course.model';
 import { GradingScale } from 'src/app/shared/models/grading-scale.model';
 import { Test } from 'src/app/shared/models/test.model';
+import { DossierGradesService } from 'src/app/shared/services/dossier-grades.service';
 
 @Component({
   selector: 'erz-dossier-course-tests',
@@ -17,25 +14,7 @@ export class DossierCourseTestsComponent {
   @Input() course: Course;
   @Input() gradingScales: GradingScale[];
 
-  constructor() {}
-
-  public getFinalGradeForStudent(): FinalGrading | undefined {
-    return this.course?.FinalGrades?.find(
-      (finaleGrade) => finaleGrade.StudentId === this.studentId
-    );
-  }
-
-  public getGradingForStudent(): Grading | undefined {
-    return this.course?.Gradings?.find(
-      (grade) => grade.StudentId === this.studentId
-    );
-  }
-
-  public getGradingScaleOfCourse() {
-    return this.gradingScales?.find(
-      (gradingScale) => gradingScale.Id === this.course.GradingScaleId
-    );
-  }
+  constructor(public dossierGradeService: DossierGradesService) {}
 
   getGradingScaleOfTest(test: Test) {
     return this.gradingScales.find(
