b6fe654a86502185d80c61203eff93e31cdbe57b
Claudia Asti <asti@puzzle.ch>
2022-05-06 17:43:12 +0200

    refactor new final grade summary component, refs #337

--- src/app/shared/components/student-dossier/dossier-grades-final-grade-summary/dossier-grades-final-grade-summary.component.spec.ts
+++ src/app/shared/components/student-dossier/dossier-grades-final-grade-summary/dossier-grades-final-grade-summary.component.spec.ts
@@ -1,5 +1,8 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { buildCourse } from 'src/spec-builders';
+import { FinalGrading, Grading } from 'src/app/shared/models/course.model';
+import { Grade } from 'src/app/shared/models/grading-scale.model';
+import { buildGradingScale } from 'src/spec-builders';
 import { buildTestModuleMetadata } from 'src/spec-helpers';
 
 import { DossierGradesFinalGradeSummaryComponent } from './dossier-grades-final-grade-summary.component';
@@ -7,6 +10,7 @@ import { DossierGradesFinalGradeSummaryComponent } from './dossier-grades-final-
 describe('DossierGradesFinalGradeSummaryComponent', () => {
   let component: DossierGradesFinalGradeSummaryComponent;
   let fixture: ComponentFixture<DossierGradesFinalGradeSummaryComponent>;
+  let debugElement: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule(
@@ -19,11 +23,37 @@ describe('DossierGradesFinalGradeSummaryComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(DossierGradesFinalGradeSummaryComponent);
     component = fixture.componentInstance;
-    component.course = buildCourse(1);
+    debugElement = fixture.debugElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show designation', () => {
+    component.designation = 'course 1';
+    fixture.detectChanges();
+
+    expect(debugElement.nativeElement.textContent).toBe('course 1');
+  });
+
+  it('should show designation and grade', () => {
+    component.designation = 'course 2';
+    component.finalGrade = ({ Grade: '5.5' } as unknown) as FinalGrading;
+    component.gradingScale = buildGradingScale(1, [
+      ({ Value: 5.5 } as unknown) as Grade,
+    ]);
+    fixture.detectChanges();
+
+    expect(debugElement.nativeElement.textContent).toBe('course 2 (5.5)');
+  });
+
+  it('should show designation and average', () => {
+    component.designation = 'course 3';
+    component.grading = ({ AverageTestResult: 5.2555 } as unknown) as Grading;
+    fixture.detectChanges();
+
+    expect(debugElement.nativeElement.textContent).toBe('course 3 (5.256)');
   });
 });
--- src/app/shared/components/student-dossier/dossier-grades-view/dossier-grades-view.component.spec.ts
+++ src/app/shared/components/student-dossier/dossier-grades-view/dossier-grades-view.component.spec.ts
@@ -1,8 +1,7 @@
 import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { Course } from 'src/app/shared/models/course.model';
 import { buildTestModuleMetadata } from 'src/spec-helpers';
-import { expectNotInTheDocument, expectText } from 'src/specs/expectations';
+import { expectText } from 'src/specs/expectations';
 import { DossierGradesViewComponent } from './dossier-grades-view.component';
 
 describe('DossierGradesViewComponent', () => {
@@ -31,17 +30,5 @@ describe('DossierGradesViewComponent', () => {
     fixture.detectChanges();
 
     expectText(debugElement, 'message-no-courses', 'dossier.no-courses');
-  });
-
-  it('should show designation of courses', () => {
-    const courses: Course[] = [
-      ({ Id: 111, Designation: 'course 1' } as unknown) as Course,
-      ({ Id: 222, Designation: 'course 2' } as unknown) as Course,
-    ];
-    component.courses = courses;
-    fixture.detectChanges();
-    expectText(debugElement, 'course-designation-111', 'course 1, 111');
-    expectText(debugElement, 'course-designation-222', 'course 2, 222');
-    expectNotInTheDocument(debugElement, 'message-no-courses');
   });
 });
