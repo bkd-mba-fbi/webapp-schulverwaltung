474663ca235d58dafe3be863551e34edcbd7246b
Max Burri <burri@puzzle.ch>
2022-05-06 14:00:23 +0200

    cleanup tests and make them actually test something, #337

--- src/app/shared/services/subscriptions-rest.service.spec.ts
+++ src/app/shared/services/subscriptions-rest.service.spec.ts
@@ -1,8 +1,14 @@
 import { HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-import { buildIdSubscription } from 'src/spec-builders';
+import {
+  buildIdSubscription,
+  buildSubscriptionDetail,
+} from 'src/spec-builders';
 import { buildTestModuleMetadata } from '../../../spec-helpers';
-import { IdSubscription } from '../models/subscription-detail.model';
+import {
+  IdSubscription,
+  SubscriptionDetail,
+} from '../models/subscription-detail.model';
 
 import { SubscriptionsRestService } from './subscriptions-rest.service';
 
@@ -16,50 +22,50 @@ describe('SubscriptionsRestService', () => {
     httpTestingController = TestBed.inject(HttpTestingController);
   });
 
-  describe('.getByRegistrationId', () => {
-    const data: any[] = [];
+  it('gets the list of subscription details for the given registration id', () => {
+    const data: ReadonlyArray<SubscriptionDetail> = [
+      buildSubscriptionDetail(1),
+      buildSubscriptionDetail(2),
+    ];
 
-    it('gets the list of subscription details for the given registration id', () => {
-      const url =
-        'https://eventotest.api/Subscriptions/1234/SubscriptionDetails';
+    const expectedUrl =
+      'https://eventotest.api/Subscriptions/1234/SubscriptionDetails';
 
-      service
-        .getListByRegistrationId(1234)
-        .subscribe((result) => expect(result).toBe(data));
+    let result: ReadonlyArray<SubscriptionDetail> = [];
+    service
+      .getListByRegistrationId(1234)
+      .subscribe((response) => (result = response));
 
-      httpTestingController
-        .expectOne((req) => req.url === url, url)
-        .flush(data);
-      httpTestingController.verify();
-    });
+    httpTestingController
+      .expectOne(({ url }) => url === expectedUrl, expectedUrl)
+      .flush(data);
+
+    httpTestingController.verify();
+    expect(result).toEqual(data);
   });
 
-  describe('getIdSubscriptionsByStudentAndCourse', () => {
-    it('should get list of IdSubscriptions for a students and its courses', () => {
-      const personId = 1;
-      const courseIds = [11, 12, 13];
-      const data: ReadonlyArray<IdSubscription> = [
-        buildIdSubscription(1, 11, personId),
-        buildIdSubscription(2, 12, personId),
-      ];
+  it('should get list of IdSubscriptions for a students and its courses', () => {
+    const personId = 1;
+    const courseIds = [11, 12, 13];
+    const data: ReadonlyArray<IdSubscription> = [
+      buildIdSubscription(1, 11, personId),
+      buildIdSubscription(2, 12, personId),
+    ];
 
-      const expectedUrl = `https://eventotest.api/Subscriptions/?filter.PersonId==1&filter.EventId==11,12,13`;
+    const expectedUrl = `https://eventotest.api/Subscriptions/?filter.PersonId==1&filter.EventId==11,12,13`;
 
-      let result: ReadonlyArray<IdSubscription> | undefined;
-      service
-        .getIdSubscriptionsByStudentAndCourse(personId, courseIds)
-        .subscribe((response) => {
-          result = response;
-        });
+    let result: ReadonlyArray<IdSubscription> | undefined;
+    service
+      .getIdSubscriptionsByStudentAndCourse(personId, courseIds)
+      .subscribe((response) => {
+        result = response;
+      });
 
-      const request = httpTestingController.expectOne(
-        ({ url }) => url === expectedUrl,
-        expectedUrl
-      );
-      request.flush(data);
+    httpTestingController
+      .expectOne(({ url }) => url === expectedUrl, expectedUrl)
+      .flush(data);
 
-      httpTestingController.verify();
-      expect(result).toEqual(data);
-    });
+    httpTestingController.verify();
+    expect(result).toEqual(data);
   });
 });
