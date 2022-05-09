0c7235a5d5414db1f5ef42f2089041117ad7bc9a
Marc <fehlmann@puzzle.ch>
2022-05-09 11:48:46 +0200

    Fix test for dossier-grades, refs 337

--- src/app/shared/components/student-dossier/dossier-grades/dossier-grades.component.spec.ts
+++ src/app/shared/components/student-dossier/dossier-grades/dossier-grades.component.spec.ts
@@ -1,22 +1,68 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { DossierGradesService } from 'src/app/shared/services/dossier-grades.service';
-import { DossierStateService } from 'src/app/shared/services/dossier-state.service';
+import {
+  DossierPage,
+  DossierStateService,
+} from 'src/app/shared/services/dossier-state.service';
 import { StorageService } from 'src/app/shared/services/storage.service';
 import { buildTestModuleMetadata } from 'src/spec-helpers';
 
 import { DossierGradesComponent } from './dossier-grades.component';
+import { BehaviorSubject, of } from 'rxjs';
+import { Course } from '../../../models/course.model';
+import { buildCourse, buildGradingScale } from '../../../../../spec-builders';
+import { GradingScale } from '../../../models/grading-scale.model';
 
 describe('DossierGradesComponent', () => {
   let component: DossierGradesComponent;
   let fixture: ComponentFixture<DossierGradesComponent>;
 
+  let dossierStateServiceMock: DossierStateService;
+  let isOverview$: BehaviorSubject<boolean>;
+  let currentDossier$: BehaviorSubject<DossierPage>;
+
+  let dossierGradesServiceMock: DossierGradesService;
+  let loading$: BehaviorSubject<boolean>;
+  let studentCourses$: BehaviorSubject<Course[]>;
+  let gradingScales$: BehaviorSubject<readonly GradingScale[]>;
+
+  let courses = [buildCourse(1234)];
+  const gradingScales = [buildGradingScale(1)];
+
   beforeEach(async () => {
+    isOverview$ = new BehaviorSubject<boolean>(false);
+    currentDossier$ = new BehaviorSubject<DossierPage>('grades');
+
+    dossierStateServiceMock = ({
+      isOverview$,
+      currentDossier$,
+      studentId$: of(123),
+    } as unknown) as DossierStateService;
+
+    loading$ = new BehaviorSubject<any>(false);
+    studentCourses$ = new BehaviorSubject<Course[]>(courses);
+    gradingScales$ = new BehaviorSubject<readonly GradingScale[]>(
+      gradingScales
+    );
+
+    dossierGradesServiceMock = ({
+      loading$,
+      studentCourses$,
+      gradingScales$,
+    } as unknown) as DossierGradesService;
+
     await TestBed.configureTestingModule(
       buildTestModuleMetadata({
         declarations: [DossierGradesComponent],
         providers: [
-          DossierStateService,
-          DossierGradesService,
+          { provide: DossierStateService, useValue: dossierStateServiceMock },
+          {
+            provide: DossierGradesService,
+            useValue: {
+              dossierGradesServiceMock,
+              setStudentId: jasmine.createSpy('setStudentId'),
+            },
+          },
           {
             provide: StorageService,
             useValue: jasmine.createSpyObj('StorageService', ['getPayload']),
