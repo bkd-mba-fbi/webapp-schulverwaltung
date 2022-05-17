d7ae28a30fe4c990a8599859b1ecebae7e883d2d
Claudia Asti <asti@puzzle.ch>
2022-05-17 11:02:10 +0200

    refactor dossier grade edit, refs #337

--- src/app/shared/components/student-dossier/dossier-grades-edit/dossier-grades-edit.component.ts
+++ src/app/shared/components/student-dossier/dossier-grades-edit/dossier-grades-edit.component.ts
@@ -1,5 +1,14 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import {
+  debounceTime,
+  filter,
+  map,
+  Observable,
+  Subject,
+  takeUntil,
+  tap,
+} from 'rxjs';
 import {
   TestGradesResult,
   TestPointsResult,
@@ -9,45 +18,75 @@ import { DropDownItem } from 'src/app/shared/models/drop-down-item.model';
 import { Result, Test } from 'src/app/shared/models/test.model';
 import { CoursesRestService } from 'src/app/shared/services/courses-rest.service';
 
+const DEBOUNCE_TIME = 500;
+
 @Component({
   selector: 'erz-dossier-grades-edit',
   templateUrl: './dossier-grades-edit.component.html',
   styleUrls: ['./dossier-grades-edit.component.scss'],
 })
-export class DossierGradesEditComponent {
+export class DossierGradesEditComponent implements OnInit {
   @Input() test: Test;
   @Input() gradeId: Option<number>;
   @Input() gradeOptions: Option<DropDownItem[]>;
   @Input() points: number;
   @Input() studentId: number;
 
-  selectedGrade: number;
   response: UpdatedTestResultResponse;
+  maxPoints: number = 0;
+
+  private gradeSubject$: Subject<number> = new Subject<number>();
+  private pointsSubject$: Subject<string> = new Subject<string>();
+
+  grade$: Observable<number> = this.gradeSubject$.pipe(
+    debounceTime(DEBOUNCE_TIME)
+  );
+
+  points$: Observable<number> = this.pointsSubject$.pipe(
+    tap((n) => console.log('a', n)),
+    debounceTime(DEBOUNCE_TIME),
+    tap((n) => console.log('b', n)),
+    filter(this.isValid.bind(this)),
+    tap((n) => console.log('c', n)),
+    map(Number),
+    tap((n) => console.log('z', n))
+  );
+
+  destroy$ = new Subject<void>();
 
   constructor(
     public activeModal: NgbActiveModal,
     private courseService: CoursesRestService
   ) {}
 
-  onGradeChange(selectedGradeId: number): void {
-    this.selectedGrade = selectedGradeId;
-    const result: TestGradesResult = {
-      StudentIds: [this.studentId],
-      TestId: this.test.Id,
-      GradeId: selectedGradeId,
-    };
-    this.updateTestResult(result);
+  ngOnInit(): void {
+    this.maxPoints = this.test.MaxPointsAdjusted || this.test.MaxPoints!; // TODO dry up
+
+    this.grade$
+      .pipe(
+        takeUntil(this.destroy$),
+        map(this.buildRuequestBodyForGradeChange.bind(this))
+      )
+      .subscribe((body) => this.updateTestResult(body));
+
+    this.points$
+      .pipe(
+        takeUntil(this.destroy$),
+        map(this.buildRequestBodyPointsChange.bind(this))
+      )
+      .subscribe((body) => this.updateTestResult(body));
+  }
+
+  onGradeChange(gradeId: number): void {
+    this.gradeSubject$.next(gradeId);
   }
 
   onPointsChange(points: string): void {
-    // TODO debounce
-    const result: TestPointsResult = {
-      StudentIds: [this.studentId],
-      TestId: this.test.Id,
-      Points: Number(points), // TODO validate
-    };
+    this.pointsSubject$.next(points);
+  }
 
-    this.updateTestResult(result); // TODO update grade on modal
+  get updatedTestResult(): Option<Result> {
+    return this.response?.TestResults[0];
   }
 
   private updateTestResult(result: TestPointsResult | TestGradesResult): void {
@@ -58,7 +97,26 @@ export class DossierGradesEditComponent {
       });
   }
 
-  get updatedTestResult(): Option<Result> {
-    return this.response?.TestResults[0];
+  // TODO dry up ---------------------------------------------------------
+  private buildRuequestBodyForGradeChange(gradeId: number): TestGradesResult {
+    return {
+      StudentIds: [this.studentId],
+      TestId: this.test.Id,
+      GradeId: gradeId,
+    };
+  }
+
+  private buildRequestBodyPointsChange(points: number): TestPointsResult {
+    return {
+      StudentIds: [this.studentId],
+      TestId: this.test.Id,
+      Points: points,
+    };
+  }
+
+  private isValid(points: string): boolean {
+    if (points === '') return false;
+    if (isNaN(Number(points))) return false;
+    return !(Number(points) < 0 || Number(points) > this.maxPoints);
   }
 }
