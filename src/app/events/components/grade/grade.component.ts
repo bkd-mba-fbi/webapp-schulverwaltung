92fef928abc50dff49bba97fd8dfd50893efe34d
Marc <fehlmann@puzzle.ch>
2022-05-19 10:31:54 +0200

    fix disabling and enabling for grading scales, refs 336

--- src/app/events/components/grade/grade.component.ts
+++ src/app/events/components/grade/grade.component.ts
@@ -89,7 +89,7 @@ export class GradeComponent implements OnInit, OnDestroy, OnChanges {
 
   onPointsChange(points: string) {
     this.pointsSubject$.next(points);
-    this.gradingScaleDisabledSubject$.next(points?.length > 0);
+    this.gradingScaleDisabledSubject$.next(points !== null);
   }
 
   onGradeChange(gradeId: number) {
