48044ad7c103b2bfc67b8aa8d3ac06c21774d625
Marc <fehlmann@puzzle.ch>
2022-05-23 16:53:18 +0200

    add my-grade header, refs 339

--- src/app/my-grades/my-grades.module.ts
+++ src/app/my-grades/my-grades.module.ts
@@ -5,9 +5,14 @@ import { MyGradesRoutingModule } from './my-grades-routing.module';
 import { MyGradesComponent } from './components/my-grades/my-grades.component';
 import { MyGradesShowComponent } from './components/my-grades-show/my-grades-show.component';
 import { SharedModule } from '../shared/shared.module';
+import { MyGradesHeaderComponent } from './components/my-grades-header/my-grades-header.component';
 
 @NgModule({
-  declarations: [MyGradesComponent, MyGradesShowComponent],
+  declarations: [
+    MyGradesComponent,
+    MyGradesShowComponent,
+    MyGradesHeaderComponent,
+  ],
   imports: [CommonModule, MyGradesRoutingModule, SharedModule],
 })
 export class MyGradesModule {}
