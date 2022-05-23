16e165d3f673c05715aa16d07de8d6c1675e72b9
Marc <fehlmann@puzzle.ch>
2022-05-23 14:20:11 +0200

    fix tests for my-grades-show component, refs 339

--- src/app/my-grades/components/my-grades-show/my-grades-show.component.spec.ts
+++ src/app/my-grades/components/my-grades-show/my-grades-show.component.spec.ts
@@ -1,14 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MyGradesShowComponent } from './my-grades-show.component';
+import { CoursesRestService } from '../../../shared/services/courses-rest.service';
+import { StorageService } from '../../../shared/services/storage.service';
 
 describe('MyGradesShowComponent', () => {
   let component: MyGradesShowComponent;
   let fixture: ComponentFixture<MyGradesShowComponent>;
+  let coursesRestService: jasmine.SpyObj<CoursesRestService>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [MyGradesShowComponent],
+      providers: [
+        { provide: CoursesRestService, useValue: coursesRestService },
+        {
+          provide: StorageService,
+          useValue: jasmine.createSpyObj('StorageService', ['getPayload']),
+        },
+      ],
     }).compileComponents();
   });
 
