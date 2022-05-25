1a669db3c31289e85190d2300821e5f2a30cec8e
Claudia Asti <asti@puzzle.ch>
2022-05-25 16:37:52 +0200

    fix tests, refs #339

--- src/app/my-grades/components/my-grades-show/my-grades-show.component.spec.ts
+++ src/app/my-grades/components/my-grades-show/my-grades-show.component.spec.ts
@@ -3,23 +3,33 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MyGradesShowComponent } from './my-grades-show.component';
 import { CoursesRestService } from '../../../shared/services/courses-rest.service';
 import { StorageService } from '../../../shared/services/storage.service';
+import { buildTestModuleMetadata } from 'src/spec-helpers';
+import { MyGradesService } from '../../services/my-grades.service';
+import { of } from 'rxjs';
 
 describe('MyGradesShowComponent', () => {
   let component: MyGradesShowComponent;
   let fixture: ComponentFixture<MyGradesShowComponent>;
-  let coursesRestService: jasmine.SpyObj<CoursesRestService>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [MyGradesShowComponent],
-      providers: [
-        { provide: CoursesRestService, useValue: coursesRestService },
-        {
-          provide: StorageService,
-          useValue: jasmine.createSpyObj('StorageService', ['getPayload']),
-        },
-      ],
-    }).compileComponents();
+    await TestBed.configureTestingModule(
+      buildTestModuleMetadata({
+        declarations: [MyGradesShowComponent],
+        providers: [
+          {
+            provide: MyGradesService,
+            useValue: {
+              studentId$: of(1),
+              studentCourses$: of([]),
+            },
+          },
+          {
+            provide: StorageService,
+            useValue: jasmine.createSpyObj('StorageService', ['getPayload']),
+          },
+        ],
+      })
+    ).compileComponents();
   });
 
   beforeEach(() => {
