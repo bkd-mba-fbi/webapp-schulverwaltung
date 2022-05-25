d0cf6273731524896eae6f48a6717892e63a6400
Marc <fehlmann@puzzle.ch>
2022-05-25 13:57:45 +0200

    get grading scales with my-grades service, refs 339

--- src/app/my-grades/components/my-grades-show/my-grades-show.component.ts
+++ src/app/my-grades/components/my-grades-show/my-grades-show.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { MyGradesService } from '../../services/my-grades.service';
+import { DossierGradesService } from '../../../shared/services/dossier-grades.service';
 
 @Component({
   selector: 'erz-my-grades-show',
   templateUrl: './my-grades-show.component.html',
   styleUrls: ['./my-grades-show.component.scss'],
+  providers: [DossierGradesService],
 })
 export class MyGradesShowComponent {
   constructor(public myGradesService: MyGradesService) {}
