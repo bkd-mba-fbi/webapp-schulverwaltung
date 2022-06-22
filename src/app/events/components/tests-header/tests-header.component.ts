7bbf9a40f86b3aeddbffaa6cb277c2a2a592b3a9
Sandro <41326409+schefbi@users.noreply.github.com>
2022-06-22 12:12:14 +0000

    test without EventsStateService

--- src/app/events/components/tests-header/tests-header.component.ts
+++ src/app/events/components/tests-header/tests-header.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Course } from '../../../shared/models/course.model';
 import { ReportsService } from '../../../shared/services/reports.service';
-import { EventsStateService } from '../../services/events-state.service';
+//import { EventsStateService } from '../../services/events-state.service';
 
 @Component({
   selector: 'erz-tests-header',
@@ -13,9 +13,9 @@ export class TestsHeaderComponent {
   @Input() course: Course;
 
   constructor(
-    private reportsService: ReportsService,
-    private eventsStateService: EventsStateService
-  ) {}
+    private reportsService: ReportsService
+  ) //private eventsStateService: EventsStateService
+  {}
 
   loadReportUrl(): string {
     return this.reportsService.getEventReportUrl(this.course.Id);
