6edcd207a6dcbe183ff4487575a30258bbd2a5ae
Marc <fehlmann@puzzle.ch>
2022-11-09 14:14:47 +0100

    remove unnecessary import #432

--- src/app/person-search/person-search.module.ts
+++ src/app/person-search/person-search.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { PersonSearchComponent } from './components/person-search/person-search.component';
 import { PersonSearchRoutingModule } from './person-search-routing.module';
@@ -7,6 +6,6 @@ import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [PersonSearchComponent],
-  imports: [CommonModule, PersonSearchRoutingModule, SharedModule],
+  imports: [PersonSearchRoutingModule, SharedModule],
 })
 export class PersonSearchModule {}
