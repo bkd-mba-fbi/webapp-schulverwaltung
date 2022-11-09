02bd7a06ebf33c434dba1be6a49bb64612ba0de0
Marc <fehlmann@puzzle.ch>
2022-11-09 16:17:17 +0100

    add test to verify router navigate #432

--- src/app/person-search/components/person-search-header/person-search-header.component.spec.ts
+++ src/app/person-search/components/person-search-header/person-search-header.component.spec.ts
@@ -2,10 +2,14 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PersonSearchHeaderComponent } from './person-search-header.component';
 import { buildTestModuleMetadata } from '../../../../spec-helpers';
+import { Router } from '@angular/router';
 
 describe('PersonSearchHeaderComponent', () => {
   let component: PersonSearchHeaderComponent;
   let fixture: ComponentFixture<PersonSearchHeaderComponent>;
+
+  let element: HTMLElement;
+  let router: Router;
 
   beforeEach(() => {
     TestBed.configureTestingModule(
@@ -14,10 +18,25 @@ describe('PersonSearchHeaderComponent', () => {
 
     fixture = TestBed.createComponent(PersonSearchHeaderComponent);
     component = fixture.componentInstance;
+    element = fixture.debugElement.nativeElement;
     fixture.detectChanges();
+
+    router = TestBed.inject(Router);
+    spyOn(router, 'navigate');
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should build url', () => {
+    const key = 12;
+    component.navigateToDossier(key);
+    expect(router.navigate).toHaveBeenCalledWith([
+      'person-search',
+      'student',
+      key,
+      'addresses',
+    ]);
   });
 });
