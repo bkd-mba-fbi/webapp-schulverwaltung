67146369b036db48d8b2af21f521ee5fda8ac623
Claudia Asti <asti@puzzle.ch>
2022-11-29 17:09:35 +0100

    Update average grade after grade edit #499

--- src/app/shared/components/student-dossier/dossier-course-tests/dossier-course-tests.component.ts
+++ src/app/shared/components/student-dossier/dossier-course-tests/dossier-course-tests.component.ts
@@ -1,22 +1,30 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { GradingScale } from 'src/app/shared/models/grading-scale.model';
 import { Test } from 'src/app/shared/models/test.model';
-import { DossierGradesService } from 'src/app/shared/services/dossier-grades.service';
 import { gradingScaleOfTest, sortByDate } from '../../../../events/utils/tests';
 import { CourseWithGrades } from '../dossier-grades-view/dossier-grades-view.component';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'erz-dossier-course-tests',
   templateUrl: './dossier-course-tests.component.html',
   styleUrls: ['./dossier-course-tests.component.scss'],
 })
-export class DossierCourseTestsComponent {
+export class DossierCourseTestsComponent implements OnChanges {
   @Input() studentId: number;
   @Input() decoratedCourse: CourseWithGrades;
   @Input() gradingScales: GradingScale[];
   @Input() isEditable: boolean;
 
-  constructor(public dossierGradeService: DossierGradesService) {}
+  sortedTests$ = new BehaviorSubject<Test[]>([]);
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.decoratedCourse) {
+      this.sortedTests$.next(this.sortedTests());
+    }
+  }
+
+  constructor() {}
 
   sortedTests() {
     if (!this.decoratedCourse.course.Tests) return [];
