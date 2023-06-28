37306b14703183f390a736bfcbb452489f449c52
caebr <asti@puzzle.ch>
2023-06-28 12:07:15 +0200

    Add dashboard search #531
    * Add person search typeahead to dashboard #531
    
    * Add custom search placeholder #531
    
    * Add styling to search box #531
    
    * Remove unnecessary typeahead initial value #531
    
    * Extract dashboard search to component #531

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { dossierRoute } from '../shared/components/student-dossier/dossier-route';
 
 const routes: Routes = [
   {
     path: '',
     component: DashboardComponent,
   },
+  dossierRoute,
 ];
 
 @NgModule({
