fcd82b512909776e8775f460b26d5094fdc4d83d
caebr <asti@puzzle.ch>
2023-06-28 12:07:15 +0200

    Add dashboard search #531
    * Add person search typeahead to dashboard #531
    
    * Add custom search placeholder #531
    
    * Add styling to search box #531
    
    * Remove unnecessary typeahead initial value #531
    
    * Extract dashboard search to component #531

---
+++ src/app/dashboard/components/dashboard-search/dashboard-search.component.spec.ts
@@ -0,0 +1,41 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+
+import { DashboardSearchComponent } from './dashboard-search.component';
+import { buildTestModuleMetadata } from '../../../../spec-helpers';
+
+describe('DashboardSearchComponent', () => {
+  let component: DashboardSearchComponent;
+  let fixture: ComponentFixture<DashboardSearchComponent>;
+  let router: Router;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule(
+      buildTestModuleMetadata({
+        declarations: [DashboardSearchComponent],
+      })
+    ).compileComponents();
+
+    fixture = TestBed.createComponent(DashboardSearchComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
+    router = TestBed.inject(Router);
+    spyOn(router, 'navigate');
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('navigates to dossier with given id', () => {
+    const key = 12;
+    component.navigateToDossier(key);
+    expect(router.navigate).toHaveBeenCalledWith([
+      'dashboard',
+      'student',
+      key,
+      'addresses',
+    ]);
+  });
+});
---
+++ src/app/dashboard/components/dashboard-search/dashboard-search.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { StudentsRestService } from '../../../shared/services/students-rest.service';
+import { DropDownItem } from '../../../shared/models/drop-down-item.model';
+
+@Component({
+  selector: 'erz-dashboard-search',
+  templateUrl: './dashboard-search.component.html',
+  styleUrls: ['./dashboard-search.component.scss'],
+})
+export class DashboardSearchComponent {
+  constructor(
+    public studentsRestService: StudentsRestService,
+    private router: Router
+  ) {}
+
+  navigateToDossier(key: DropDownItem['Key']) {
+    const id = Number(key);
+    this.router.navigate(['dashboard', 'student', id, 'addresses']);
+  }
+}
