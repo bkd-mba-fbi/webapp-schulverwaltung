9782ed6a7f86d0397389a32f400e0b139d9a442a
Claudia Asti <asti@puzzle.ch>
2023-07-04 14:28:56 +0200

    Add new dashboard action component #530

---
+++ src/app/dashboard/components/dashboard-action/dashboard-action.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'erz-dashboard-action',
+  templateUrl: './dashboard-action.component.html',
+  styleUrls: ['./dashboard-action.component.scss'],
+})
+export class DashboardActionComponent {
+  constructor() {}
+}
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -4,9 +4,14 @@ import { SharedModule } from '../shared/shared.module';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { DashboardSearchComponent } from './components/dashboard-search/dashboard-search.component';
+import { DashboardActionComponent } from './components/dashboard-action/dashboard-action.component';
 
 @NgModule({
-  declarations: [DashboardComponent, DashboardSearchComponent],
+  declarations: [
+    DashboardComponent,
+    DashboardSearchComponent,
+    DashboardActionComponent,
+  ],
   imports: [SharedModule, DashboardRoutingModule],
 })
 export class DashboardModule {}
