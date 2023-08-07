8b31ab6a67e78a33aae7bdfd87d88c97bc1352f1
Claudia Asti <asti@puzzle.ch>
2023-08-07 12:05:45 +0200

    Add teacher substitutions rest service #530

---
+++ src/app/shared/services/teacher-substitutions-rest.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { RestService } from './rest.service';
+import { Settings, SETTINGS } from '../../settings';
+import { TeacherSubstitution } from '../models/teacher-substitution.model';
+import { Observable, map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TeacherSubstitutionsRestService extends RestService<
+  typeof TeacherSubstitution
+> {
+  constructor(http: HttpClient, @Inject(SETTINGS) settings: Settings) {
+    super(http, settings, TeacherSubstitution, 'TeacherSubstitutions');
+  }
+
+  getTeacherSubstitution(
+    substitutionId: number
+  ): Observable<Option<TeacherSubstitution>> {
+    return this.getList({ params: { 'filter.Id': `=${substitutionId}` } }).pipe(
+      map((substitutions) => substitutions[0] || null)
+    );
+  }
+}
