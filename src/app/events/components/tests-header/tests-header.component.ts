79d74161b0c0b049a9e0b39dd165848826aeb2ea
Mathis Hofer <hofer@puzzle.ch>
2023-11-08 14:27:26 +0100

    Fix multiple test reports loading #610

--- src/app/events/components/tests-header/tests-header.component.ts
+++ src/app/events/components/tests-header/tests-header.component.ts
@@ -1,28 +1,35 @@
-import { Component, Input } from "@angular/core";
-import { Observable } from "rxjs";
+import { Component, Input, OnChanges, SimpleChanges } from "@angular/core";
+import { BehaviorSubject, of, startWith, switchMap } from "rxjs";
 import { EventsRestService } from "src/app/shared/services/events-rest.service";
 import { Course } from "../../../shared/models/course.model";
-import {
-  ReportInfo,
-  ReportsService,
-} from "../../../shared/services/reports.service";
+import { ReportsService } from "../../../shared/services/reports.service";
 
 @Component({
   selector: "erz-tests-header",
   templateUrl: "./tests-header.component.html",
   styleUrls: ["./tests-header.component.scss"],
 })
-export class TestsHeaderComponent {
+export class TestsHeaderComponent implements OnChanges {
   // TODO: Get course over state service
   @Input() course: Course;
+
+  private course$ = new BehaviorSubject<Option<Course>>(null);
+  reports$ = this.course$.pipe(
+    switchMap((course) =>
+      course ? this.reportsService.getCourseReports(this.course.Id) : of([]),
+    ),
+    startWith([]),
+  );
 
   constructor(
     private reportsService: ReportsService,
     private eventsRestService: EventsRestService,
   ) {}
 
-  loadReports(): Observable<ReadonlyArray<ReportInfo>> {
-    return this.reportsService.getCourseReports(this.course.Id);
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.course) {
+      this.course$.next(changes.course.currentValue);
+    }
   }
 
   getDesignation() {
