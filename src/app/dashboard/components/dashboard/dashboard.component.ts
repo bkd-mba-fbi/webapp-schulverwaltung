4114da6af7b28fefae663b7f9d8f9f5421118bc2
Mathis Hofer <hofer@puzzle.ch>
2024-04-29 16:45:32 +0200

    Fix dashboard dossier absences #669

--- src/app/dashboard/components/dashboard/dashboard.component.ts
+++ src/app/dashboard/components/dashboard/dashboard.component.ts
@@ -1,13 +1,7 @@
-import { AsyncPipe, NgIf } from "@angular/common";
 import { ChangeDetectionStrategy, Component } from "@angular/core";
-import { TranslateModule } from "@ngx-translate/core";
-import { SpinnerComponent } from "../../../shared/components/spinner/spinner.component";
-import { LetDirective } from "../../../shared/directives/let.directive";
+import { RouterOutlet } from "@angular/router";
 import { ConfirmAbsencesSelectionService } from "../../../shared/services/confirm-absences-selection.service";
 import { DashboardService } from "../../services/dashboard.service";
-import { DashboardActionsComponent } from "../dashboard-actions/dashboard-actions.component";
-import { DashboardSearchComponent } from "../dashboard-search/dashboard-search.component";
-import { DashboardTimetableComponent } from "../dashboard-timetable/dashboard-timetable.component";
 
 @Component({
   selector: "bkd-dashboard",
@@ -15,18 +9,9 @@ import { DashboardTimetableComponent } from "../dashboard-timetable/dashboard-ti
   styleUrls: ["./dashboard.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [
-    LetDirective,
-    NgIf,
-    SpinnerComponent,
-    DashboardSearchComponent,
-    DashboardActionsComponent,
-    DashboardTimetableComponent,
-    AsyncPipe,
-    TranslateModule,
-  ],
+  imports: [RouterOutlet],
   providers: [DashboardService, ConfirmAbsencesSelectionService],
 })
 export class DashboardComponent {
-  constructor(public dashboardService: DashboardService) {}
+  constructor() {}
 }
