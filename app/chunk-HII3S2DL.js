6e73ddccc95e78bed2b0bc551e265f79cd84c260
hupf <mathis@fsfe.org>
2024-11-25 10:22:25 +0000

    Deploying to gh-pages from @ bkd-mba-fbi/webapp-schulverwaltung@ab166a5a3e07e6f9bfe439e092a63c34784e4a1d 🚀

--- app/chunk-HII3S2DL.js
+++
@@ -1,1 +0,0 @@
-import{Ac as n,Fc as o,Hc as r,zc as t}from"./chunk-XTHSYQMN.js";var i=o({Key:r([n,t]),Value:t});export{i as a};
---
+++ app/chunk-OFB4XAK2.js
@@ -0,0 +1,1 @@
+import{Cc as t,Dc as n,Ic as o,Kc as r}from"./chunk-IFJN5CDK.js";var i=o({Key:r([n,t]),Value:t});export{i as a};
