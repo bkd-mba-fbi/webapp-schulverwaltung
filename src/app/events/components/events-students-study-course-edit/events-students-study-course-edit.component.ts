891fbf61ae4467ef4877912c6ac19b2419da3bfc
Mathis Hofer <hofer@puzzle.ch>
2024-12-04 14:26:10 +0100

    WIP

--- src/app/events/components/events-students-study-course-edit/events-students-study-course-edit.component.ts
+++ src/app/events/components/events-students-study-course-edit/events-students-study-course-edit.component.ts
@@ -1,13 +1,19 @@
-import { AsyncPipe, JsonPipe } from "@angular/common";
-import { ChangeDetectionStrategy, Component, effect } from "@angular/core";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  effect,
+  signal,
+} from "@angular/core";
 import { toSignal } from "@angular/core/rxjs-interop";
+import { FormBuilder, FormGroup, ReactiveFormsModule } from "@angular/forms";
 import { ActivatedRoute, Router } from "@angular/router";
+import { TranslateModule } from "@ngx-translate/core";
 import { StudyCourseSelectionService } from "../../services/study-course-selection.service";
 
 @Component({
   selector: "bkd-events-students-study-course-edit",
   standalone: true,
-  imports: [AsyncPipe, JsonPipe],
+  imports: [ReactiveFormsModule, TranslateModule],
   templateUrl: "./events-students-study-course-edit.component.html",
   styleUrl: "./events-students-study-course-edit.component.scss",
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -16,17 +22,30 @@ export class EventsStudentsStudyCourseEditComponent {
   selectedIds = toSignal(this.selectionService.selectedIds$, {
     initialValue: [],
   });
+  saving = signal(false);
+  formGroup = this.createFormGroup();
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
+    private fb: FormBuilder,
     public selectionService: StudyCourseSelectionService,
   ) {
     effect(() => {
       if (this.selectedIds().length === 0) {
         // No selection present, redirect to list
-        void this.router.navigate(["../"], { relativeTo: this.route });
+        this.cancel();
       }
     });
+  }
+
+  onSubmit(): void {}
+
+  cancel(): void {
+    void this.router.navigate(["../"], { relativeTo: this.route });
+  }
+
+  private createFormGroup(): FormGroup {
+    return this.fb.group({});
   }
 }
