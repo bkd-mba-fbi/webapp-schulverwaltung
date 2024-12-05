43e0ed32983130ecda91c1d31c515e62c0a46714
Claudia Asti <asti@puzzle.ch>
2024-12-05 10:42:52 +0100

    Prepare return link #716
    WIP #716

--- src/app/events/components/events-students-study-course-detail/events-students-study-course-detail.component.ts
+++ src/app/events/components/events-students-study-course-detail/events-students-study-course-detail.component.ts
@@ -1,11 +1,16 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { AvatarComponent } from "../../../shared/components/avatar/avatar.component";
+import { BacklinkComponent } from "../../../shared/components/backlink/backlink.component";
 
 @Component({
   selector: "bkd-events-students-study-course-detail",
   standalone: true,
-  imports: [],
+  imports: [BacklinkComponent, AvatarComponent],
   templateUrl: "./events-students-study-course-detail.component.html",
   styleUrl: "./events-students-study-course-detail.component.scss",
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class EventsStudentsStudyCourseDetailComponent {}
+export class EventsStudentsStudyCourseDetailComponent {
+  constructor(private route: ActivatedRoute) {}
+}
