406f84ffabbd24a303735542f4e94b1d367f1fe3
Claudia Asti <asti@puzzle.ch>
2024-12-05 14:20:21 +0100

    Add basic person info #716

--- src/app/events/components/events-students-study-course-detail/events-students-study-course-detail.component.ts
+++ src/app/events/components/events-students-study-course-detail/events-students-study-course-detail.component.ts
@@ -1,16 +1,30 @@
+import { DatePipe } from "@angular/common";
 import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { toSignal } from "@angular/core/rxjs-interop";
 import { ActivatedRoute } from "@angular/router";
-import { AvatarComponent } from "../../../shared/components/avatar/avatar.component";
+import { map, switchMap } from "rxjs";
 import { BacklinkComponent } from "../../../shared/components/backlink/backlink.component";
+import { PersonsRestService } from "../../../shared/services/persons-rest.service";
 
 @Component({
   selector: "bkd-events-students-study-course-detail",
   standalone: true,
-  imports: [BacklinkComponent, AvatarComponent],
+  imports: [BacklinkComponent, DatePipe],
   templateUrl: "./events-students-study-course-detail.component.html",
   styleUrl: "./events-students-study-course-detail.component.scss",
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EventsStudentsStudyCourseDetailComponent {
-  constructor(private route: ActivatedRoute) {}
+  person = toSignal(
+    this.route.paramMap.pipe(
+      map((params) => params.get("id")),
+      switchMap((id) => this.personService.get(Number(id))),
+    ),
+    { initialValue: null },
+  );
+
+  constructor(
+    private route: ActivatedRoute,
+    private personService: PersonsRestService,
+  ) {}
 }
