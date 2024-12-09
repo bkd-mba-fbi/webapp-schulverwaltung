b9194f2cccc478d3d340880a15a6c90ae89c51de
Mathis Hofer <hofer@puzzle.ch>
2024-12-09 11:35:15 +0100

    Determine correct students count for study courses #713

--- src/app/shared/services/subscriptions-rest.service.ts
+++ src/app/shared/services/subscriptions-rest.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Inject, Injectable } from "@angular/core";
+import * as t from "io-ts";
 import { Observable } from "rxjs";
 import { map, switchMap } from "rxjs/operators";
 import { SETTINGS, Settings } from "../../settings";
@@ -20,33 +21,58 @@ export class SubscriptionsRestService extends RestService<typeof Subscription> {
     personId: number,
     eventIds: number[],
   ): Observable<ReadonlyArray<number>> {
-    return this.getList({
-      params: {
-        "filter.PersonId": `=${personId}`,
-        "filter.EventId": `;${eventIds}`,
-      },
-    }).pipe(
-      switchMap(decodeArray(Identifiable)),
-      map((result) => result.map((i) => i.Id)),
-    );
+    return this.http
+      .get<unknown>(`${this.baseUrl}/`, {
+        params: {
+          "filter.PersonId": `=${personId}`,
+          "filter.EventId": `;${eventIds}`,
+        },
+      })
+      .pipe(
+        switchMap(decodeArray(Identifiable)),
+        map((result) => result.map((i) => i.Id)),
+      );
+  }
+
+  getSubscriptionCountsByEvents(
+    eventIds: ReadonlyArray<number>,
+  ): Observable<Dict<number>> {
+    return this.http
+      .get<unknown>(`${this.baseUrl}/`, {
+        params: {
+          "filter.EventId": `;${eventIds.join(";")}`,
+          fields: ["Id", "EventId"].join(","),
+        },
+      })
+      .pipe(
+        switchMap(decodeArray(t.type({ Id: t.number, EventId: t.number }))),
+        map((subscriptions) =>
+          subscriptions.reduce<Dict<number>>((acc, s) => {
+            acc[s.EventId] = acc[s.EventId] ? acc[s.EventId] + 1 : 1;
+            return acc;
+          }, {}),
+        ),
+      );
   }
 
   getSubscriptionsByCourse(
     eventId: number,
     additionalParams?: Dict<string>,
   ): Observable<ReadonlyArray<Subscription>> {
-    return this.getList({
-      params: {
-        "filter.EventId": `=${eventId}`,
-        ...additionalParams,
-        fields: [
-          "Id",
-          "EventId",
-          "EventDesignation",
-          "PersonId",
-          "Status",
-        ].join(","),
-      },
-    }).pipe(switchMap(decodeArray(Subscription)));
+    return this.http
+      .get<unknown>(`${this.baseUrl}/`, {
+        params: {
+          "filter.EventId": `=${eventId}`,
+          ...additionalParams,
+          fields: [
+            "Id",
+            "EventId",
+            "EventDesignation",
+            "PersonId",
+            "Status",
+          ].join(","),
+        },
+      })
+      .pipe(switchMap(decodeArray(Subscription)));
   }
 }
