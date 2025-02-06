fd09f2aca55bec8eb21bca7ef24f5dced530269b
Claudia Asti <asti@puzzle.ch>
2025-02-06 17:18:21 +0100

    Scaffold state service #758

--- src/app/import-subscription-details/components/import-subscription-details/import-subscription-details.component.ts
+++ src/app/import-subscription-details/components/import-subscription-details/import-subscription-details.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
+import { ImportSubscriptionDetailsStateService } from "../../services/import-subscription-details-state.service";
 
 @Component({
   selector: "bkd-import-subscription-details",
@@ -7,5 +8,6 @@ import { RouterOutlet } from "@angular/router";
   templateUrl: "./import-subscription-details.component.html",
   styleUrl: "./import-subscription-details.component.scss",
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [ImportSubscriptionDetailsStateService],
 })
 export class ImportSubscriptionDetailsComponent {}
