72c23a3b01e0ef745a63934651143174f2f1ff3e
llorentelemmc <71637828+llorentelemmc@users.noreply.github.com>
2025-02-06 22:49:49 +0000

    Deploying to gh-pages from @ bkd-mba-fbi/webapp-schulverwaltung@2cd6d99a75a202083b95ec4f7adb0af400310a21 🚀

--- app/chunk-5HFGLKFS.js
+++
@@ -1,1 +0,0 @@
-import{Hc as t,Ic as n,Nc as o,Pc as r}from"./chunk-SFIM5H6S.js";var i=o({Key:r([n,t]),Value:t});export{i as a};
---
+++ app/chunk-XKNLLORS.js
@@ -0,0 +1,1 @@
+import{Ic as t,Jc as n,Oc as o,Qc as r}from"./chunk-S3H6YSFW.js";var i=o({Key:r([n,t]),Value:t});export{i as a};
