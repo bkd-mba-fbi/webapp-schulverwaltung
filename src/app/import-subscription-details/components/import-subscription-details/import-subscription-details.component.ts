b1ca91de0760f7d7397bbcd276a88f9f5940e6f9
Claudia Asti <asti@puzzle.ch>
2025-02-06 17:07:04 +0100

    Add child component #758

--- src/app/import-subscription-details/components/import-subscription-details/import-subscription-details.component.ts
+++ src/app/import-subscription-details/components/import-subscription-details/import-subscription-details.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: "bkd-import-subscription-details",
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: "./import-subscription-details.component.html",
   styleUrl: "./import-subscription-details.component.scss",
   changeDetection: ChangeDetectionStrategy.OnPush,
