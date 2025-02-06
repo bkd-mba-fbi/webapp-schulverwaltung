657eb07e9c820d3b4d04135f79fcf716891c5449
Claudia Asti <asti@puzzle.ch>
2025-02-06 15:55:19 +0100

    Scaffold new module import subscription details #758

---
+++ src/app/import-subscription-details/import-subscription-details.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from "@angular/router";
+import { ImportSubscriptionDetailsComponent } from "./components/import-subscription-details/import-subscription-details.component";
+
+export const IMPORT_SUBSCRIPTION_DETAILS_ROUTES: Routes = [
+  {
+    path: "",
+    component: ImportSubscriptionDetailsComponent,
+  },
+];
