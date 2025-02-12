91fa173877dd3639c4416d743a37e97c8c6d4858
llorentelemmc <llorente@puzzle.ch>
2025-02-12 16:11:07 +0100

    Change styling of dossier addresses

--- src/app/shared/components/student-dossier/dossier-contact/dossier-contact.component.ts
+++ src/app/shared/components/student-dossier/dossier-contact/dossier-contact.component.ts
@@ -1,18 +1,9 @@
 import { AsyncPipe } from "@angular/common";
 import { ChangeDetectionStrategy, Component, inject } from "@angular/core";
-import {
-  NgbAccordionBody,
-  NgbAccordionCollapse,
-  NgbAccordionDirective,
-  NgbAccordionHeader,
-  NgbAccordionItem,
-  NgbCollapse,
-} from "@ng-bootstrap/ng-bootstrap";
 import { TranslatePipe } from "@ngx-translate/core";
 import { DossierStateService } from "src/app/shared/services/dossier-state.service";
 import { StudentDossierAddressComponent } from "../student-dossier-address/student-dossier-address.component";
 import { StudentDossierApprenticeshipCompanyComponent } from "../student-dossier-apprenticeship-company/student-dossier-apprenticeship-company.component";
-import { StudentDossierEntryHeaderComponent } from "../student-dossier-entry-header/student-dossier-entry-header.component";
 import { StudentDossierLegalRepresentativeComponent } from "../student-dossier-legal-representative/student-dossier-legal-representative.component";
 
 @Component({
@@ -22,13 +13,6 @@ import { StudentDossierLegalRepresentativeComponent } from "../student-dossier-l
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
     StudentDossierAddressComponent,
-    NgbAccordionDirective,
-    NgbAccordionItem,
-    NgbAccordionHeader,
-    StudentDossierEntryHeaderComponent,
-    NgbCollapse,
-    NgbAccordionCollapse,
-    NgbAccordionBody,
     StudentDossierLegalRepresentativeComponent,
     StudentDossierApprenticeshipCompanyComponent,
     AsyncPipe,
