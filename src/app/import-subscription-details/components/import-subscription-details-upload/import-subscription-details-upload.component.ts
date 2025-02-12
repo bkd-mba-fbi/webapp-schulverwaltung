20678c4953458769782343219a5b3800aeb1c487
Claudia Asti <asti@puzzle.ch>
2025-02-12 13:52:29 +0100

    convert to json and show as table #758

--- src/app/import-subscription-details/components/import-subscription-details-upload/import-subscription-details-upload.component.ts
+++ src/app/import-subscription-details/components/import-subscription-details-upload/import-subscription-details-upload.component.ts
@@ -1,33 +1,21 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { AsyncPipe } from "@angular/common";
+import { ChangeDetectionStrategy, Component, inject } from "@angular/core";
 import { TranslatePipe } from "@ngx-translate/core";
-import { read, utils } from "xlsx";
-
-export type SubscriptionDetailData = {
-  eventId: number;
-  personId: number;
-  subscriptionDetailId: number;
-  value: string;
-  personEmail?: string;
-};
+import { ImportSubscriptionDetailsStateService } from "../../services/import-subscription-details-state.service";
 
 @Component({
   selector: "bkd-import-subscription-details-upload",
-  imports: [TranslatePipe],
+  imports: [TranslatePipe, AsyncPipe],
   templateUrl: "./import-subscription-details-upload.component.html",
   styleUrl: "./import-subscription-details-upload.component.scss",
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ImportSubscriptionDetailsUploadComponent {
-  rows: SubscriptionDetailData[] = [];
+  stateService = inject(ImportSubscriptionDetailsStateService);
 
-  async onFileSelect(event: Event) {
-    const input = event.target as HTMLInputElement;
-    const file = input.files?.[0];
-    const ab = await file?.arrayBuffer();
-    const wb = read(ab);
-    const ws = wb.Sheets[wb.SheetNames[0]];
-    const data = utils.sheet_to_json<SubscriptionDetailData>(ws);
-    this.rows = data;
-    console.log(data);
+  onFileInput(files: FileList | null): void {
+    if (files) {
+      this.stateService.setFile(files.item(0));
+    }
   }
 }
