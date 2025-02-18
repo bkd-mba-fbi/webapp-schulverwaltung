3e798c3ed1d1e5215600b1fa053cb08487488129
llorentelemmc <llorente@puzzle.ch>
2025-02-18 11:38:30 +0100

    Create reusable sortable header component

---
+++ src/app/shared/components/sortable-header/sortable-header.component.ts
@@ -0,0 +1,35 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from "@angular/core";
+import { PrimarySortKey } from "../../../open-absences/services/open-absences.service";
+import { SortCriteria } from "../../utils/sort";
+
+@Component({
+  selector: "bkd-sortable-header",
+  imports: [],
+  templateUrl: "./sortable-header.component.html",
+  styleUrl: "./sortable-header.component.scss",
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SortableHeaderComponent {
+  @Input() label = "";
+  @Input() sortKey: PrimarySortKey;
+  @Input() currentSort: SortCriteria<string>;
+  @Output() sortRequested = new EventEmitter<PrimarySortKey>();
+
+  get sortDirectionCharacter(): string {
+    if (!this.currentSort) return "";
+    if (this.currentSort.primarySortKey !== this.sortKey) return "";
+
+    // Change to: If ascending is true, show '↓', else '↑'?
+    return this.currentSort.ascending ? "↓" : "↑";
+  }
+
+  onHeaderClick() {
+    this.sortRequested.emit(this.sortKey);
+  }
+}
