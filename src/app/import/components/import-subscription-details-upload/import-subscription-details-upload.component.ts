d1e8014af3fbb27cefd3ec1f1a1e88ab78ce85f3
Claudia Asti <asti@puzzle.ch>
2025-02-19 17:37:32 +0100

    Show error or success notification #758

--- src/app/import/components/import-subscription-details-upload/import-subscription-details-upload.component.ts
+++ src/app/import/components/import-subscription-details-upload/import-subscription-details-upload.component.ts
@@ -5,12 +5,13 @@ import {
   signal,
 } from "@angular/core";
 import { TranslatePipe } from "@ngx-translate/core";
+import { NotificationComponent } from "../../../shared/components/notification/notification.component";
 import { ProgressComponent } from "../../../shared/components/progress/progress.component";
 import { UploadProgress } from "../../services/import-upload-subscription-details.service";
 
 @Component({
   selector: "bkd-import-subscription-details-upload",
-  imports: [TranslatePipe, ProgressComponent],
+  imports: [TranslatePipe, ProgressComponent, NotificationComponent],
   templateUrl: "./import-subscription-details-upload.component.html",
   styleUrl: "./import-subscription-details-upload.component.scss",
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -26,5 +27,10 @@ export class ImportSubscriptionDetailsUploadComponent {
       error: 5,
       total: 100,
     });
+  }
+
+  retry() {
+    // TODO implement
+    console.log("retry");
   }
 }
