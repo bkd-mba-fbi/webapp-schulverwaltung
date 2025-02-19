f253a7de1ed552276dd2e632a6c1e85864160b3a
Claudia Asti <asti@puzzle.ch>
2025-02-19 14:41:55 +0100

    Show progress bar with mocked progress #758

--- src/app/import/components/import-subscription-details-upload/import-subscription-details-upload.component.ts
+++ src/app/import/components/import-subscription-details-upload/import-subscription-details-upload.component.ts
@@ -1,10 +1,30 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Signal,
+  signal,
+} from "@angular/core";
+import { TranslatePipe } from "@ngx-translate/core";
+import { ProgressComponent } from "../../../shared/components/progress/progress.component";
+import { UploadProgress } from "../../services/import-upload-subscription-details.service";
 
 @Component({
   selector: "bkd-import-subscription-details-upload",
-  imports: [],
+  imports: [TranslatePipe, ProgressComponent],
   templateUrl: "./import-subscription-details-upload.component.html",
   styleUrl: "./import-subscription-details-upload.component.scss",
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ImportSubscriptionDetailsUploadComponent {}
+export class ImportSubscriptionDetailsUploadComponent {
+  progress: Signal<UploadProgress>;
+
+  constructor() {
+    // TODO implement
+    this.progress = signal<UploadProgress>({
+      uploading: 75,
+      success: 20,
+      error: 5,
+      total: 100,
+    });
+  }
+}
