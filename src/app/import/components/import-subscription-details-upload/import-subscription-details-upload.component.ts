e09f751c8e0e75fda2f85e84d960ba65e460a756
Claudia Asti <asti@puzzle.ch>
2025-02-19 13:40:46 +0100

    Add import subscription details upload component #758

---
+++ src/app/import/components/import-subscription-details-upload/import-subscription-details-upload.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from "@angular/core";
+
+@Component({
+  selector: "bkd-import-subscription-details-upload",
+  imports: [],
+  templateUrl: "./import-subscription-details-upload.component.html",
+  styleUrl: "./import-subscription-details-upload.component.scss",
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ImportSubscriptionDetailsUploadComponent {}
