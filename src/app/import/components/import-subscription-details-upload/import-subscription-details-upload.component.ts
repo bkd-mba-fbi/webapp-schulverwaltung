8862540fb3fcf8a149cbd4c3d0ae4ed0ffb6b262
Mathis Hofer <hofer@puzzle.ch>
2025-03-04 08:54:36 +0100

    Prepare upload service/component #758

--- src/app/import/components/import-subscription-details-upload/import-subscription-details-upload.component.ts
+++ src/app/import/components/import-subscription-details-upload/import-subscription-details-upload.component.ts
@@ -1,13 +1,16 @@
 import {
   ChangeDetectionStrategy,
   Component,
-  Signal,
-  signal,
+  WritableSignal,
+  inject,
 } from "@angular/core";
+import { Router } from "@angular/router";
 import { TranslatePipe } from "@ngx-translate/core";
 import { NotificationComponent } from "../../../shared/components/notification/notification.component";
 import { ProgressComponent } from "../../../shared/components/progress/progress.component";
-import { UploadProgress } from "../../services/import-upload-subscription-details.service";
+import { ImportStateService } from "../../services/import-state.service";
+import { ImportUploadSubscriptionDetailsService } from "../../services/import-upload-subscription-details.service";
+import { SubscriptionDetailImportEntry } from "../../services/import-validate-subscription-details.service";
 
 @Component({
   selector: "bkd-import-subscription-details-upload",
@@ -17,20 +20,42 @@ import { UploadProgress } from "../../services/import-upload-subscription-detail
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ImportSubscriptionDetailsUploadComponent {
-  progress: Signal<UploadProgress>;
+  private router = inject(Router);
+  private stateService = inject(ImportStateService);
+  private uploadService = inject(ImportUploadSubscriptionDetailsService);
+
+  importEntries: WritableSignal<
+    Option<ReadonlyArray<SubscriptionDetailImportEntry>>
+  > = this.stateService.importEntries;
+  progress = this.uploadService.progress;
 
   constructor() {
-    // TODO implement
-    this.progress = signal<UploadProgress>({
-      uploading: 75,
-      success: 20,
-      error: 5,
-      total: 100,
-    });
+    const importEntries = this.importEntries();
+    if (importEntries === null || importEntries.length === 0) {
+      this.navigateToFilePage();
+      return;
+    }
+
+    this.upload(importEntries);
   }
 
   retry() {
     // TODO implement
     console.log("retry");
+
+    this.upload(this.importEntries() ?? [], { retryFailedOnly: true });
+  }
+
+  private upload(
+    importEntries: ReadonlyArray<SubscriptionDetailImportEntry>,
+    options: { retryFailedOnly?: boolean } = { retryFailedOnly: false },
+  ): void {
+    void this.uploadService
+      .upload(importEntries, options)
+      .then((entries) => this.importEntries.set(entries));
+  }
+
+  private navigateToFilePage(): void {
+    void this.router.navigate(["/import"]);
   }
 }
--- src/app/import/services/import-upload-subscription-details.service.ts
+++ src/app/import/services/import-upload-subscription-details.service.ts
@@ -1,4 +1,10 @@
-import { Injectable } from "@angular/core";
+import { Injectable, signal } from "@angular/core";
+import { Observable, delay, firstValueFrom, of, switchMap } from "rxjs";
+import { executeWithMaxConcurrency } from "src/app/shared/utils/observable";
+import { SubscriptionDetailImportError } from "../utils/subscription-details/error";
+import { SubscriptionDetailImportEntry } from "./import-validate-subscription-details.service";
+
+const MAX_CONCURRENT_REQUESTS = 20;
 
 export type UploadProgress = {
   uploading: number;
@@ -11,5 +17,96 @@ export type UploadProgress = {
   providedIn: "root",
 })
 export class ImportUploadSubscriptionDetailsService {
+  progress = signal({
+    uploading: 0,
+    success: 0,
+    error: 0,
+    total: 0,
+  });
+
   constructor() {}
+
+  upload(
+    validatedEntries: ReadonlyArray<SubscriptionDetailImportEntry>,
+    { retryFailedOnly = false }: { retryFailedOnly?: boolean } = {},
+  ): Promise<ReadonlyArray<SubscriptionDetailImportEntry>> {
+    let importEntries = validatedEntries.filter(
+      (entry) => entry.validationStatus === "valid",
+    );
+    if (retryFailedOnly) {
+      importEntries = importEntries.filter(
+        (entry) => entry.importStatus === "error",
+      );
+    }
+    return this.persistEntries(importEntries).then(() => validatedEntries);
+  }
+
+  private async persistEntries(
+    entries: ReadonlyArray<SubscriptionDetailImportEntry>,
+  ): Promise<void> {
+    this.prepare(entries);
+
+    await firstValueFrom(
+      executeWithMaxConcurrency(
+        entries,
+        this.persistEntry.bind(this),
+        MAX_CONCURRENT_REQUESTS,
+      ),
+    );
+  }
+
+  private prepare(entries: ReadonlyArray<SubscriptionDetailImportEntry>): void {
+    // Mark all entries as "importing"
+    entries.forEach((entry) => {
+      entry.importStatus = "importing";
+      entry.importError = null;
+    });
+
+    // Reset the progress
+    this.progress.set({
+      uploading: entries.length,
+      success: 0,
+      error: 0,
+      total: entries.length,
+    });
+  }
+
+  private persistEntry(entry: SubscriptionDetailImportEntry): Observable<void> {
+    // Don't upload entries with an unchanged value
+    if (entry.entry.value === entry.data.subscriptionDetail?.Value) {
+      this.markSuccessEntry(entry);
+      return of();
+    }
+
+    // TODO:
+    // - Perform PUT request
+    // - Retry
+    // - Handle success & update progress
+    // - Handle error & update progress
+    return of().pipe(switchMap(delay(1000))) as Observable<void>;
+  }
+
+  private markSuccessEntry(entry: SubscriptionDetailImportEntry): void {
+    entry.importStatus = "success";
+
+    this.progress.update(({ uploading, success, ...rest }) => ({
+      uploading: uploading - 1,
+      success: success + 1,
+      ...rest,
+    }));
+  }
+
+  private markErrorEntry(
+    entry: SubscriptionDetailImportEntry,
+    error: SubscriptionDetailImportError,
+  ): void {
+    entry.importStatus = "success";
+    entry.importError = error;
+
+    this.progress.update(({ uploading, error, ...rest }) => ({
+      uploading: uploading - 1,
+      error: error + 1,
+      ...rest,
+    }));
+  }
 }
