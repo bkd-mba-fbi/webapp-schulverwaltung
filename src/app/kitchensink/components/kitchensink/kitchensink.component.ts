e6bfc49a56db9e89e4b5f90e42396ca67dd3f644
Mathis Hofer <hofer@puzzle.ch>
2025-04-10 11:45:09 +0200

    Render value & properties #812

--- src/app/kitchensink/components/kitchensink/kitchensink.component.ts
+++ src/app/kitchensink/components/kitchensink/kitchensink.component.ts
@@ -1,4 +1,6 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { JsonPipe } from "@angular/common";
+import { ChangeDetectionStrategy, Component, inject } from "@angular/core";
+import { FormBuilder } from "@angular/forms";
 import {
   SubscriptionDetail,
   SubscriptionDetailType,
@@ -7,47 +9,47 @@ import { SubscriptionDetailFieldComponent } from "../../../shared/components/sub
 
 @Component({
   selector: "bkd-kitchensink",
-  imports: [SubscriptionDetailFieldComponent],
+  imports: [JsonPipe, SubscriptionDetailFieldComponent],
   templateUrl: "./kitchensink.component.html",
   styleUrl: "./kitchensink.component.scss",
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class KitchensinkComponent {
-  subscriptionDetails: ReadonlyArray<SubscriptionDetail> = [
-    build({
+  subscriptionDetails: ReadonlyArray<Partial<SubscriptionDetail>> = [
+    {
       VssDesignation: "Heading",
       Id: "1",
       VssStyle: "HE",
-    }),
-    build({
+    },
+    {
       VssDesignation: "Description",
       Id: "2",
       VssStyle: "BE",
       Value:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    }),
-    build({
+    },
+    {
       VssDesignation: "Textfield",
       Id: "3",
       VssTypeId: SubscriptionDetailType.ShortText,
       VssStyle: "TX",
       Value: "Lorem ipsum",
-    }),
-    build({
+    },
+    {
       VssDesignation: "Textarea",
       Id: "4",
       VssTypeId: SubscriptionDetailType.Text,
       VssStyle: "TX",
       Value: "Lorem ipsum",
-    }),
-    build({
+    },
+    {
       VssDesignation: "Number field",
       Id: "5",
       VssTypeId: SubscriptionDetailType.Int,
       VssStyle: "TX",
       Value: 42,
-    }),
-    build({
+    },
+    {
       VssDesignation: "Listbox Select",
       Id: "6",
       VssStyle: "LB",
@@ -58,8 +60,8 @@ export class KitchensinkComponent {
       ],
       ShowAsRadioButtons: false,
       Value: 2,
-    }),
-    build({
+    },
+    {
       VssDesignation: "Listbox Radios",
       Id: "7",
       VssStyle: "LB",
@@ -70,8 +72,8 @@ export class KitchensinkComponent {
       ],
       ShowAsRadioButtons: true,
       Value: 2,
-    }),
-    build({
+    },
+    {
       VssDesignation: "Combobox",
       Id: "8",
       VssStyle: "CB",
@@ -81,7 +83,7 @@ export class KitchensinkComponent {
         { Key: "Banana", Value: "Banana", IsActive: false },
       ],
       Value: "Strawberry",
-    }),
+    },
   ];
 
   subscriptionDetailsReadonly = this.subscriptionDetails.map((detail) => ({
@@ -93,25 +95,27 @@ export class KitchensinkComponent {
     ...detail,
     VssInternet: "M",
   }));
-}
 
-function build(detail: Partial<SubscriptionDetail>): SubscriptionDetail {
-  return {
-    Id: "1",
-    SubscriptionId: 1,
-    VssId: 1,
-    EventId: 1,
-    IdPerson: 1,
-    DropdownItems: [],
-    ShowAsRadioButtons: false,
-    VssType: "",
-    VssTypeId: SubscriptionDetailType.ShortText,
-    VssStyle: "TX",
-    VssInternet: "E",
-    VssDesignation: "",
-    Tooltip: "",
-    Value: null,
-    Sort: "",
-    ...detail,
-  };
+  private fb = inject(FormBuilder);
+
+  build(detail: Partial<SubscriptionDetail>): SubscriptionDetail {
+    return {
+      Id: "1",
+      SubscriptionId: 1,
+      VssId: 1,
+      EventId: 1,
+      IdPerson: 1,
+      DropdownItems: [],
+      ShowAsRadioButtons: false,
+      VssType: "",
+      VssTypeId: SubscriptionDetailType.ShortText,
+      VssStyle: "TX",
+      VssInternet: "E",
+      VssDesignation: "",
+      Tooltip: "",
+      Value: null,
+      Sort: "",
+      ...detail,
+    };
+  }
 }
