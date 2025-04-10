37f08c38fbeb8d003c295db343f9dfc548f919b4
llorentelemmc <llorente@puzzle.ch>
2025-04-10 11:33:46 +0200

    Use select component with dummy data #816

--- src/app/events/components/evaluation/evaluation-dialog/evaluation-dialog.component.ts
+++ src/app/events/components/evaluation/evaluation-dialog/evaluation-dialog.component.ts
@@ -1,14 +1,16 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  computed,
   inject,
   signal,
 } from "@angular/core";
 import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
+import { SelectComponent } from "../../../../shared/components/select/select.component";
 
 @Component({
   selector: "bkd-evaluation-dialog",
-  imports: [],
+  imports: [SelectComponent],
   templateUrl: "./evaluation-dialog.component.html",
   styleUrl: "./evaluation-dialog.component.scss",
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -16,8 +18,22 @@ import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
 export class EvaluationDialogComponent {
   activeModal = inject(NgbActiveModal);
 
-  grades = ["1.0", "2.0", "3.0", "4.0", "5.0", "6.0"];
-  selectedGrade = signal<string | null>(null);
+  grades = [
+    { Key: 1, Value: "1.1" },
+    { Key: 2, Value: "4.5" },
+    { Key: 3, Value: "7.0" },
+    { Key: 4, Value: "8.5" },
+    { Key: 5, Value: "3.0" },
+  ];
+
+  selectedGradeKey = signal<number | null>(null);
+
+  options = this.grades;
+
+  selectedGrade = computed(() => {
+    const key = this.selectedGradeKey();
+    return this.options.find((o) => o.Key === key) ?? null;
+  });
 
   cancel(): void {
     this.activeModal.dismiss();
@@ -27,5 +43,9 @@ export class EvaluationDialogComponent {
     if (this.selectedGrade()) {
       this.activeModal.close(this.selectedGrade());
     }
+  }
+
+  setGradeByKey(key: number | null) {
+    this.selectedGradeKey.set(key);
   }
 }
