1063ed44ef1fd47ace2350d0b9d788f95a521d38
llorentelemmc <llorente@puzzle.ch>
2025-04-10 14:02:14 +0200

    Fix test #816

--- src/app/events/components/evaluation/evaluation-dialog/evaluation-dialog.component.spec.ts
+++ src/app/events/components/evaluation/evaluation-dialog/evaluation-dialog.component.spec.ts
@@ -1,17 +1,34 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
+import { TranslateModule } from "@ngx-translate/core";
+import { GradingScale } from "../../../../shared/models/grading-scale.model";
 import { EvaluationDialogComponent } from "./evaluation-dialog.component";
 
 describe("EvaluationDialogComponent", () => {
   let component: EvaluationDialogComponent;
   let fixture: ComponentFixture<EvaluationDialogComponent>;
 
+  const mockGradingScale: GradingScale = {
+    Id: 1,
+    Grades: [
+      {
+        Id: 1,
+        Designation: "1.0",
+        Value: 1.0,
+        Sort: null,
+      },
+    ],
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [EvaluationDialogComponent],
+      imports: [EvaluationDialogComponent, TranslateModule.forRoot()],
+      providers: [NgbActiveModal],
     }).compileComponents();
 
     fixture = TestBed.createComponent(EvaluationDialogComponent);
     component = fixture.componentInstance;
+    fixture.componentRef.setInput("gradingScale", mockGradingScale);
     fixture.detectChanges();
   });
 
