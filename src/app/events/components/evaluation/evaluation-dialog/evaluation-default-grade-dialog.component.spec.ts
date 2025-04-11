778c69f655218b4b08532a4d7ef7c1b28a327703
llorentelemmc <llorente@puzzle.ch>
2025-04-11 13:16:04 +0200

    Return grade object and rename modal #816

--- src/app/events/components/evaluation/evaluation-dialog/evaluation-default-grade-dialog.component.spec.ts
+++ src/app/events/components/evaluation/evaluation-dialog/evaluation-default-grade-dialog.component.spec.ts
@@ -2,11 +2,11 @@ import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
 import { TranslateModule } from "@ngx-translate/core";
 import { GradingScale } from "../../../../shared/models/grading-scale.model";
-import { EvaluationDialogComponent } from "./evaluation-dialog.component";
+import { EvaluationDefaultGradeDialogComponent } from "./evaluation-default-grade-dialog.component";
 
 describe("EvaluationDialogComponent", () => {
-  let component: EvaluationDialogComponent;
-  let fixture: ComponentFixture<EvaluationDialogComponent>;
+  let component: EvaluationDefaultGradeDialogComponent;
+  let fixture: ComponentFixture<EvaluationDefaultGradeDialogComponent>;
 
   const mockGradingScale: GradingScale = {
     Id: 1,
@@ -22,11 +22,14 @@ describe("EvaluationDialogComponent", () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [EvaluationDialogComponent, TranslateModule.forRoot()],
+      imports: [
+        EvaluationDefaultGradeDialogComponent,
+        TranslateModule.forRoot(),
+      ],
       providers: [NgbActiveModal],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(EvaluationDialogComponent);
+    fixture = TestBed.createComponent(EvaluationDefaultGradeDialogComponent);
     component = fixture.componentInstance;
     fixture.componentRef.setInput("gradingScale", mockGradingScale);
     fixture.detectChanges();
