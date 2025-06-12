5e9680c66de0acb5210444ed4cc66062b850f766
Clara Llorente Lemm <71637828+llorentelemmc@users.noreply.github.com>
2025-06-12 14:51:59 +0100

    Fix textarea resize in mobile view #842

--- src/app/shared/directives/textarea-autosize.directive.ts
+++ src/app/shared/directives/textarea-autosize.directive.ts
@@ -3,12 +3,15 @@ import {
   Directive,
   ElementRef,
   HostListener,
+  OnDestroy,
 } from "@angular/core";
 
 @Directive({
   selector: "textarea[bkdTextareaAutosize]",
 })
-export class TextareaAutosizeDirective implements AfterViewInit {
+export class TextareaAutosizeDirective implements AfterViewInit, OnDestroy {
+  private observer?: IntersectionObserver;
+
   constructor(private elementRef: ElementRef) {}
 
   @HostListener(":input")
@@ -16,9 +19,37 @@ export class TextareaAutosizeDirective implements AfterViewInit {
     this.resize();
   }
 
+  @HostListener("window:resize")
+  onWindowResize() {
+    this.resize();
+  }
+
   ngAfterViewInit() {
     if (this.elementRef.nativeElement.scrollHeight) {
       setTimeout(() => this.resize());
+    }
+    // Using IntersectionObserver to ensure proper resizing, when the textarea is initially hidden and then becomes visible when the column is selected in the dropdown.
+    this.observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            this.resize();
+          }
+        });
+      },
+      {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.1,
+      },
+    );
+
+    this.observer.observe(this.elementRef.nativeElement);
+  }
+
+  ngOnDestroy() {
+    if (this.observer) {
+      this.observer.unobserve(this.elementRef.nativeElement);
     }
   }
 
