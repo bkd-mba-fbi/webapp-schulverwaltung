26723ce4eadc9ed87bd6bfc1ae0ec15573e6235a
Stephan Girod <girod@puzzle.ch>
2025-06-13 16:51:09 +0200

    Init statistic values #829

--- src/app/events/components/evaluation/evaluation-statistic/evaluation-statistic.component.ts
+++ src/app/events/components/evaluation/evaluation-statistic/evaluation-statistic.component.ts
@@ -1,4 +1,9 @@
-import { ChangeDetectionStrategy, Component, inject } from "@angular/core";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+} from "@angular/core";
 import { TranslatePipe } from "@ngx-translate/core";
 import { EvaluationChartComponent } from "src/app/events/components/evaluation/evaluation-statistic/evaluation-chart/evaluation-chart.component";
 import { SpinnerComponent } from "src/app/shared/components/spinner/spinner.component";
@@ -19,4 +24,19 @@ import { EvaluationHeaderComponent } from "../evaluation-header/evaluation-heade
 })
 export class EvaluationStatisticComponent {
   state = inject(EvaluationStateService);
+
+  entries = computed(() =>
+    this.state.entries().filter((entry) => entry.grade?.Value != null),
+  );
+
+  average = computed(() => {
+    if (this.entries().length === 0) {
+      return 0;
+    }
+    const sum = this.entries().reduce(
+      (acc, entry) => acc + (entry.grade?.Value ?? 0),
+      0,
+    );
+    return sum / this.entries().length;
+  });
 }
