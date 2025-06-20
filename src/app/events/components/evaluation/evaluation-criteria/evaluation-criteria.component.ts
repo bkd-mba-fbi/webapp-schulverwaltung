b00b24c23811086bf976221d8c16d281fe6590e1
Stephan Girod <girod@puzzle.ch>
2025-06-20 16:59:10 +0200

    Show dynamic title for set-criteria in mobile view #828

--- src/app/events/components/evaluation/evaluation-criteria/evaluation-criteria.component.ts
+++ src/app/events/components/evaluation/evaluation-criteria/evaluation-criteria.component.ts
@@ -1,11 +1,15 @@
+import { NgClass } from "@angular/common";
 import {
   ChangeDetectionStrategy,
   Component,
   HostBinding,
+  computed,
+  inject,
   input,
   model,
   output,
 } from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
 import {
   EvaluationEntry,
   EvaluationSubscriptionDetail,
@@ -14,12 +18,13 @@ import { SubscriptionDetailFieldComponent } from "../../../../shared/components/
 
 @Component({
   selector: "bkd-evaluation-criteria",
-  imports: [SubscriptionDetailFieldComponent],
+  imports: [NgClass, SubscriptionDetailFieldComponent],
   templateUrl: "./evaluation-criteria.component.html",
   styleUrl: "./evaluation-criteria.component.scss",
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EvaluationCriteriaComponent {
+  private translate = inject(TranslateService);
   entry = input.required<EvaluationEntry>();
   visible = model.required<boolean>();
   subscriptionDetailChange = output<EvaluationSubscriptionDetail>();
@@ -28,6 +33,20 @@ export class EvaluationCriteriaComponent {
   get visibleClass() {
     return this.visible();
   }
+
+  heading = computed(() => {
+    const defaultHeading = this.translate.instant("evaluation.set-criteria");
+    if (this.entry().criteria.length === 0) {
+      return defaultHeading;
+    }
+    const detail = this.entry().criteria.find(
+      (c) => c.detail.VssStyle === "HE",
+    )?.detail;
+    if (!detail) {
+      return defaultHeading;
+    }
+    return detail.VssDesignation;
+  });
 
   toggle() {
     this.visible.update((visible) => !visible);
