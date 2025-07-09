a050992124fb93a8fbdc5df1ffdeeea5d2acf63d
Claudia Asti <asti@puzzle.ch>
2025-07-09 15:59:03 +0200

    chore: fix lint errors #889

--- src/app/my-absences/components/my-absences-confirm/my-absences-confirm.component.ts
+++ src/app/my-absences/components/my-absences-confirm/my-absences-confirm.component.ts
@@ -1,21 +1,11 @@
 import { AsyncPipe, NgClass } from "@angular/common";
 import { ChangeDetectionStrategy, Component, inject } from "@angular/core";
-import {
-  FormsModule,
-  ReactiveFormsModule,
-  UntypedFormBuilder,
-} from "@angular/forms";
-import { Router } from "@angular/router";
-import { TranslatePipe, TranslateService } from "@ngx-translate/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { TranslatePipe } from "@ngx-translate/core";
 import { flatten, uniq } from "lodash-es";
 import { map } from "rxjs/operators";
-import { SETTINGS, Settings } from "src/app/settings";
 import { ConfirmAbsencesSelectionService } from "src/app/shared/services/confirm-absences-selection.service";
-import { LessonPresencesUpdateRestService } from "src/app/shared/services/lesson-presences-update-rest.service";
-import { PresenceTypesService } from "src/app/shared/services/presence-types.service";
-import { StorageService } from "src/app/shared/services/storage.service";
 import { AddSpacePipe } from "../../../shared/pipes/add-space.pipe";
-import { ToastService } from "../../../shared/services/toast.service";
 import { MyAbsencesService } from "../../services/my-absences.service";
 import { MyAbsencesAbstractConfirmComponent } from "./my-absences-abstract-confirm.component";
 
@@ -44,25 +34,7 @@ export class MyAbsencesConfirmComponent extends MyAbsencesAbstractConfirmCompone
   protected confirmationStateId = this.settings.unconfirmedAbsencesRefreshTime;
 
   constructor() {
-    const fb = inject(UntypedFormBuilder);
-    const router = inject(Router);
-    const toastService = inject(ToastService);
-    const translate = inject(TranslateService);
-    const presenceTypesService = inject(PresenceTypesService);
-    const updateService = inject(LessonPresencesUpdateRestService);
-    const storageService = inject(StorageService);
-    const settings = inject<Settings>(SETTINGS);
-
-    super(
-      fb,
-      router,
-      toastService,
-      translate,
-      presenceTypesService,
-      updateService,
-      storageService,
-      settings,
-    );
+    super();
   }
 
   protected override onSaveSuccess(): void {
--- src/app/my-absences/components/my-absences-confirm/my-absences-report-confirm.component.ts
+++ src/app/my-absences/components/my-absences-confirm/my-absences-report-confirm.component.ts
@@ -1,22 +1,12 @@
 import { AsyncPipe, NgClass } from "@angular/common";
 import { ChangeDetectionStrategy, Component, inject } from "@angular/core";
-import {
-  FormsModule,
-  ReactiveFormsModule,
-  UntypedFormBuilder,
-} from "@angular/forms";
-import { Router } from "@angular/router";
-import { TranslatePipe, TranslateService } from "@ngx-translate/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { TranslatePipe } from "@ngx-translate/core";
 import { flatten, uniq } from "lodash-es";
 import { Observable } from "rxjs";
 import { map, take } from "rxjs/operators";
-import { SETTINGS, Settings } from "src/app/settings";
 import { PresenceType } from "src/app/shared/models/presence-type.model";
-import { LessonPresencesUpdateRestService } from "src/app/shared/services/lesson-presences-update-rest.service";
-import { PresenceTypesService } from "src/app/shared/services/presence-types.service";
-import { StorageService } from "src/app/shared/services/storage.service";
 import { AddSpacePipe } from "../../../shared/pipes/add-space.pipe";
-import { ToastService } from "../../../shared/services/toast.service";
 import { MyAbsencesReportSelectionService } from "../../services/my-absences-report-selection.service";
 import { MyAbsencesReportStateService } from "../../services/my-absences-report-state.service";
 import { MyAbsencesAbstractConfirmComponent } from "./my-absences-abstract-confirm.component";
@@ -46,25 +36,7 @@ export class MyAbsencesReportConfirmComponent extends MyAbsencesAbstractConfirmC
   protected confirmationStateId = this.settings.checkableAbsenceStateId;
 
   constructor() {
-    const fb = inject(UntypedFormBuilder);
-    const router = inject(Router);
-    const toastService = inject(ToastService);
-    const translate = inject(TranslateService);
-    const presenceTypesService = inject(PresenceTypesService);
-    const updateService = inject(LessonPresencesUpdateRestService);
-    const storageService = inject(StorageService);
-    const settings = inject<Settings>(SETTINGS);
-
-    super(
-      fb,
-      router,
-      toastService,
-      translate,
-      presenceTypesService,
-      updateService,
-      storageService,
-      settings,
-    );
+    super();
   }
 
   protected override getHalfDayType(): Observable<Option<PresenceType>> {
