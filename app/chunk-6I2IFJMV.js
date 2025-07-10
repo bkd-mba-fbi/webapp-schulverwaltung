457ba218bdbfeeafd264ead2b5b2637b06931dab
caebr <claudia@asti.ch>
2025-07-10 13:53:47 +0000

    Deploying to gh-pages from @ bkd-mba-fbi/webapp-schulverwaltung@3e7e0f2c95a4c83e1334e15d99471c1d4bf463af 🚀

--- app/chunk-6I2IFJMV.js
+++
@@ -1,1 +0,0 @@
-import{Ad as n,Bd as r,Fd as e,Id as o,zd as t}from"./chunk-6PRZEEKG.js";var s=e({Key:o([n,t]),Value:t}),m=e({Key:o([n,t]),Value:t,IsActive:r});export{s as a,m as b};
---
+++ app/chunk-SNNR7FJ6.js
@@ -0,0 +1,1 @@
+import{Cd as t,Dd as n,Ed as r,Id as e,Ld as o}from"./chunk-GP622B77.js";var s=e({Key:o([n,t]),Value:t}),m=e({Key:o([n,t]),Value:t,IsActive:r});export{s as a,m as b};
