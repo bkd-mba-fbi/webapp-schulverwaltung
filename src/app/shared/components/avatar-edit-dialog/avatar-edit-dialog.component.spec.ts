922b8f9f1def7d120e334fadb590d6f1452424a2
Mathis Hofer <hofer@puzzle.ch>
2026-04-16 10:38:15 +0200

    Avoid duplicate photo request in avatar edit dialog #946

--- src/app/shared/components/avatar-edit-dialog/avatar-edit-dialog.component.spec.ts
+++ src/app/shared/components/avatar-edit-dialog/avatar-edit-dialog.component.spec.ts
@@ -95,7 +95,9 @@ describe("AvatarEditDialogComponent", () => {
     ).not.toHaveBeenCalled();
     await component.proceed();
     fixture.detectChanges();
-    expect(additionalInformationsServiceMock.uploadPhoto).toHaveBeenCalled();
+    expect(additionalInformationsServiceMock.uploadPhoto).toHaveBeenCalledTimes(
+      1,
+    );
     expect(component.step()).toBe("uploading");
     expect(component.canCancel()).toBe(true);
     expect(component.canProceed()).toBe(false);
